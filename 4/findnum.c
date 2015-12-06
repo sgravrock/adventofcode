6200101109a2d077ab074c47f52ba0b07a731623
Steve Gravrock <sdg@panix.com>
2015-12-05 22:04:53 -0800

    Day 4 part 2

--- 4/findnum.c
+++ 4/findnum.c
@@ -4,17 +4,20 @@
 #include <limits.h>
 #include <openssl/md5.h>
 
-static int match(const char *prefix, int n);
+static int match(const char *prefix, int suffix, int nzeros);
+static int has_leading_zeros(const unsigned char *md, int nzeros);
 
 int main(int argc, char **argv)
 {
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s prefix\n", argv[0]);
+	int nzeros;
+
+	if (argc != 3 || sscanf(argv[2], "%u", &nzeros) != 1) {
+		fprintf(stderr, "Usage: %s prefix nzeros\n", argv[0]);
 		return 1;
 	}
 
 	for (int i = 0; i < INT_MAX;  i++) {
-		if (match(argv[1], i)) {
+		if (match(argv[1], i, nzeros)) {
 			printf("%d\n", i);
 			return 0;
 		}
@@ -23,18 +26,27 @@ int main(int argc, char **argv)
 	return 1;
 }
 
-static int match(const char *prefix, int n) {
+static int match(const char *prefix, int suffix, int nzeros) {
 	char *msg;
-	asprintf(&msg, "%s%d", prefix, n);
+	asprintf(&msg, "%s%d", prefix, suffix);
 	unsigned char md[MD5_DIGEST_LENGTH];
 	MD5((const unsigned char *)msg, strlen(msg), md);
 	free(msg);
+	return has_leading_zeros(md, nzeros);
+}
 
-	char checkbuf[7];
+static int has_leading_zeros(const unsigned char *md, int nzeros) {
+	char buf[MD5_DIGEST_LENGTH * 2 + 1] = {0};
 
-	for (int i = 0; i < 3; i++) {
-		sprintf(checkbuf + 2*i, "%02x", (unsigned int)md[i]);
+	for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+		sprintf(buf + 2*i, "%02x", (unsigned int)md[i]);
 	}
 
-	return strncmp(checkbuf, "00000", 5) == 0;
+	for (int i = 0; i < nzeros; i++) {
+		if (buf[i] != '0') {
+			return 0;
+		}
+	}
+
+	return 1;
 }
