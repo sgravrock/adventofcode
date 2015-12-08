f0763dd72faf6a706417142025d8473767493036
Steve Gravrock <sdg@panix.com>
2015-12-07 22:27:37 -0800

    Made day 6 part 1 faster

--- 6/part1.cc
+++ 6/part1.cc
@@ -3,13 +3,13 @@ using std::cin;
 using std::cout;
 using std::cerr;
 using std::endl;
-#include <set>
 #include <map>
 #include <utility>
 using std::pair;
 #include <string>
 using std::string;
 #include <sstream>
+#include <stdlib.h>
 
 enum action { on, off, toggle };
 typedef pair<unsigned long, unsigned long> coordinate;
@@ -18,12 +18,14 @@ typedef std::map<string, action> action_map;
 pair<string, action> find_action(string line, action_map actions);
 
 int main(void) {
-	std::set<coordinate> lit;
+	unsigned char lit[1000][1000];
+	bzero(lit, sizeof lit);
 	action_map actions;
 	actions["turn on"] = on;
 	actions["turn off"] = off;
 	actions["toggle"] = toggle;
 	string line;
+	long counter = 0;
 
 	while (std::getline(cin, line)) {
 		auto i = find_action(line, actions);
@@ -38,26 +40,30 @@ int main(void) {
 
 		for (long x = x0; x <= x1; x++) {
 			for (long y = y0; y <= y1; y++) {
-				auto key = std::make_pair(x, y);
+				auto old = lit[x][y];
 				switch (i.second) {
 					case on:
-						lit.insert(key);
+						lit[x][y] = 1;
+						if (!old) counter++;
 						break;
 					case off:
-						lit.erase(key);
+						lit[x][y] = 0;
+						if (old) counter--;
 						break;
 					case toggle:
-						if (lit.find(key) == lit.end()) {
-							lit.insert(key);
+						if (lit[x][y]) {
+							lit[x][y] = 0;
+							counter--;
 						} else {
-							lit.erase(key);
+							lit[x][y] = 1;
+							counter++;
 						}
 				}
 			}
 		}
 	}
 
-	cout << lit.size() << endl;
+	cout << counter << endl;
 }
 
 pair<string, action> find_action(string line, action_map actions) {
