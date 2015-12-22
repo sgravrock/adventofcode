c07b553f4d9c9e1d40f1815edfb64165e3ba1171
Steve Gravrock <sdg@panix.com>
2015-12-22 12:03:47 -0800

    Day 11 part 1

---
+++ 11/part1.py
@@ -0,0 +1,56 @@
+from __future__ import generators
+import re
+
+def nextpass(p):
+	for candidate in string_sequence(p):
+		if is_valid(candidate):
+			return candidate
+
+def is_valid(p):
+	return has_straight(p) and all_good_chars(p) and has_pairs(p)
+
+def has_straight(p):
+	for i in xrange(0, len(p) - 2):
+		if is_straight(p, i):
+			return True
+	return False
+
+def is_straight(p, start):
+	for i in xrange(start + 1, start + 3):
+		if p[i] != nextchar(p[i-1]):
+			return False
+	return True
+
+def nextchar(c):
+	if c == 'z':
+		return 'a'
+	else:
+		return chr(ord(c) + 1)
+
+def all_good_chars(p):
+	for c in "iol":
+		if c in p:
+			return False
+	return True
+
+def has_pairs(p):
+	return re.search(r"(.)\1.*(.)\2", p) != None
+
+def string_sequence(s):
+	while True:
+		s = nextstring(s)
+		yield s
+
+def nextstring(s):
+	sl = list(s)
+
+	for i in xrange(len(sl) - 1, -1, -1):
+		if sl[i] != 'z':
+			sl[i] = nextchar(sl[i])
+			return "".join(sl)
+		else:
+			for j in xrange(i, len(sl)):
+				sl[i] = 'a'
+
+if __name__ == "__main__":
+	print nextpass("hepxcrrq")
---
+++ 11/test_part1.py
@@ -0,0 +1,38 @@
+import unittest
+import sys
+sys.path.append(".")
+import part1
+
+class TestPart1(unittest.TestCase):
+	def test_has_straight(self):
+		self.assertTrue(part1.has_straight("hijklmmn"))
+		self.assertTrue(part1.has_straight("abc"))
+		self.assertFalse(part1.has_straight("abd"))
+		self.assertFalse(part1.has_straight("qbc"))
+		self.assertTrue(part1.has_straight("zab"))
+
+	def test_good_chars(self):
+		self.assertTrue(part1.all_good_chars("hjkmmn"))
+		self.assertFalse(part1.all_good_chars("hijklmmn"))
+
+	def test_has_pairs(self):
+		self.assertTrue(part1.has_pairs("abbceffg"))
+		self.assertTrue(part1.has_pairs("bbff"))
+		self.assertTrue(part1.has_pairs("abcdffaa"))
+		self.assertFalse(part1.has_pairs("abbcegjk"))
+
+	def test_is_valid(self):
+		self.assertTrue(part1.is_valid("abcdffaa"))
+		self.assertTrue(part1.is_valid("ghjaabcc"))
+		self.assertFalse(part1.is_valid("hijklmmn"))
+		self.assertFalse(part1.is_valid("abbceffg"))
+
+	def test_string_sequence(self):
+		seq = part1.string_sequence("azy")
+		self.assertEqual(seq.next(), "azz")
+		self.assertEqual(seq.next(), "baa")
+		self.assertEqual(seq.next(), "bab")
+
+	def test_nextpass(self):
+		self.assertEqual(part1.nextpass("abcdefgh"), "abcdffaa")
+		self.assertEqual(part1.nextpass("ghijklmn"), "ghjaabcc")
