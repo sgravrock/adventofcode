6af09ef0ab015cd81376ff587092a9dd365231ae
Steve Gravrock <sdg@panix.com>
2017-01-07 22:26:57 -0800

    Day 17 part 1 (Rust)

--- rust/day17-1/src/main.rs
+++ rust/day17-1/src/main.rs
@@ -1,31 +1,70 @@
 extern crate crypto;
 use crypto::md5::Md5;
 use crypto::digest::Digest;
+use std::collections::VecDeque;
+use std::rc::Rc;
 
 fn main() {
+	println!("{:?}", shortest_path("pslxynzg"));
 }
 
-/*
 fn shortest_path(passcode: &str) -> Option<String> {
-	bfs((0,0), |p| p == (3,3), next_directions)
-		.map(|path| path.collect());
+	let mut queue = VecDeque::new();
+	queue.push_back(Rc::new(Node { pos: (0,0), prev: None }));
+
+	while queue.len() > 0 {
+		let node = queue.pop_front().unwrap();
+		let path = node.path();
+
+		if node.pos == (3,3) {
+			return Some(path);
+		}
+
+		for d in next_directions(passcode, node.pos, &path) {
+			queue.push_back(Rc::new(Node {
+				pos: next_pos(node.pos, d),
+				prev: Some((d, node.clone()))
+			}));
+		}
+	}
+
+	None
+}
+
+struct Node {
+	pos: (i32, i32),
+	prev: Option<(char, Rc<Node>)>
+}
+
+impl Node {
+	fn path(&self) -> String {
+		let mut s = String::new();
+		self.path_into(&mut s);
+		s
+	}
+
+	fn path_into(&self, s: &mut String) {
+		match self.prev {
+			Some((d, ref p)) => {
+				p.path_into(s);
+				s.push(d);
+			},
+			None => {}
+		}
+	}
 }
 
 #[test]
 fn test_shortest_path() {
-	assert_eq!("DDRRRD", shortest_path("ihgpwlah"));
-	assert_eq!("DDUDRLRRUDRD", shortest_path("kglvqrro"));
-	assert_eq!("DRURDRUDDLLDLUURRDULRLDUUDDDRR", shortest_path("ulqzkmiv"));
+	assert_eq!(Some("DDRRRD".to_string()), shortest_path("ihgpwlah"));
+	assert_eq!(Some("DDUDRLRRUDRD".to_string()), shortest_path("kglvqrro"));
+	assert_eq!(Some("DRURDRUDDLLDLUURRDULRLDUUDDDRR".to_string()),
+		shortest_path("ulqzkmiv"));
 
 }
-*/
 
-fn position(path: &[char]) -> (i32, i32) {
-	path.iter().fold((0,0), next_pos)
-}
-
-fn next_pos(start: (i32, i32), dir: &char) -> (i32, i32) {
-	match *dir {
+fn next_pos(start: (i32, i32), dir: char) -> (i32, i32) {
+	match dir {
 		'U' => (start.0, start.1 - 1),
 		'D' => (start.0, start.1 + 1),
 		'L' => (start.0 - 1, start.1),
@@ -34,19 +73,9 @@ fn next_pos(start: (i32, i32), dir: &char) -> (i32, i32) {
 	}
 }
 
-#[test]
-fn test_position() {
-	assert_eq!((0,0), position(&vec![]));
-	assert_eq!((1,0), position(&vec!['R']));
-	assert_eq!((1,1), position(&vec!['R', 'D']));
-	assert_eq!((0,1), position(&vec!['R', 'D', 'L']));
-	assert_eq!((0,0), position(&vec!['R', 'D', 'L', 'U']));
-}
-
-fn next_directions(passcode: &str, position: (i32, i32), path: &[char]) -> Vec<char> {
+fn next_directions(passcode: &str, position: (i32, i32), path: &str) -> Vec<char> {
 	let dirs = vec!['U', 'D', 'L', 'R'];
-	let suffix: String = path.iter().cloned().collect();
-	let hash = make_hash(&(passcode.to_string() + &suffix));
+	let hash = make_hash(&(passcode.to_string() + &path));
 	let chars = hash.chars().take(4);
 
 	dirs.into_iter()
@@ -64,7 +93,9 @@ fn next_directions(passcode: &str, position: (i32, i32), path: &[char]) -> Vec<c
 		.filter(|d| {
 			match (*d, position) {
 				('U', (_, 0)) => false,
+				('D', (_, 3)) => false,
 				('L', (0, _)) => false,
+				('R', (3, _)) => false,
 				_ => true
 			}
 		})
@@ -75,10 +106,10 @@ fn next_directions(passcode: &str, position: (i32, i32), path: &[char]) -> Vec<c
 
 #[test]
 fn test_next_directions() {
-	assert_eq!(vec!['D'], next_directions("hijkl", (0,0), &vec![]));
-	assert_eq!(vec!['U', 'R'], next_directions("hijkl", (0,1), &vec!['D']));
-	assert_eq!(vec!['R'], next_directions("hijkl", (0,1), &vec!['D', 'U']));
-	assert_eq!(0, next_directions("hijkl", (1,0), &vec!['D', 'U', 'R']).len());
+	assert_eq!(vec!['D'], next_directions("hijkl", (0,0), ""));
+	assert_eq!(vec!['U', 'R'], next_directions("hijkl", (0,1), "D"));
+	assert_eq!(vec!['R'], next_directions("hijkl", (0,1), "DU"));
+	assert_eq!(0, next_directions("hijkl", (1,0), "DUR").len());
 }
 
 fn make_hash(input: &str) -> String {
