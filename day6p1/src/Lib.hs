d03b2c180d71971002ee0073da5f6f17fc391662
Steve Gravrock <sdg@panix.com>
2017-06-10 13:33:22 -0700

    Day 6 part 1: working

--- day6p1/src/Lib.hs
+++ day6p1/src/Lib.hs
@@ -3,26 +3,71 @@ module Lib
     , Point(..)
     , XRange(..)
     , YRange(..)
+    , CmdType(..)
     , Cmd(..)
+    , runScript
+    , parseCmd
     , execute
     ) where
 
 import qualified Data.Set as Set
+import Data.List.Split
 
 isNice :: String -> Bool
 isNice _ = False
 
 data Point = Point Int Int deriving(Show, Ord, Eq)
-data XRange = XRange Int Int deriving(Show)
-data YRange = YRange Int Int deriving(Show)
-data Cmd = On | Off | Toggle
+data XRange = XRange Int Int deriving(Show, Eq)
+data YRange = YRange Int Int deriving(Show, Eq)
+data CmdType = On | Off | Toggle deriving(Show, Eq)
+data Cmd = Cmd CmdType XRange YRange deriving(Show, Eq)
 
 type Grid = Set.Set Point
 
-execute :: Grid -> Cmd -> XRange -> YRange -> Grid
-execute s cmd xr yr = update s xr yr (cmdFn cmd)
+runScript :: [String] -> Int
+runScript strings =
+    let commands = map parseCmd strings
+        input = Set.empty
+        output = foldl execute input commands
+    in Set.size output
 
-cmdFn :: Cmd -> (Bool -> Bool)
+parseCmd :: String -> Cmd
+parseCmd s
+    | words!!0 == "toggle" = parseToggle words
+    | otherwise = parseOnOff words
+    where words = splitOn " " s
+
+parseToggle :: [String] -> Cmd
+parseToggle words = 
+    let (xr, yr) = parseRanges (words!!1) (words!!3)
+    in Cmd Toggle xr yr
+
+parseOnOff :: [String] -> Cmd
+parseOnOff words
+    | cmdWord == "on" = Cmd On xr yr
+    | cmdWord == "off" = Cmd Off xr yr
+    | otherwise = error ("Unrecognized command: " ++ cmdWord)
+    where
+        cmdWord = words!!1
+        (xr, yr) = parseRanges (words!!2) (words!!4)
+
+parseRanges :: String -> String -> (XRange, YRange)
+parseRanges startWord endWord =
+    let (x0, y0) = parseNumPair startWord
+        (x1, y1) = parseNumPair endWord
+    in ((XRange x0 x1), (YRange y0 y1))
+
+parseNumPair :: String -> (Int, Int)
+parseNumPair s =
+    let tokens = splitOn "," s
+        a = read (tokens!!0)
+        b = read (tokens!!1)
+    in (a, b)
+
+execute :: Grid -> Cmd -> Grid
+execute s (Cmd cmdType xr yr) = update s xr yr (cmdFn cmdType)
+
+cmdFn :: CmdType -> (Bool -> Bool)
 cmdFn On = \_ -> True
 cmdFn Off = \_ -> False
 cmdFn Toggle = \x -> not x
--- day6p1/test/Spec.hs
+++ day6p1/test/Spec.hs
@@ -10,10 +10,14 @@ setOfTwo = Set.insert (Point 0 1) setOfOrigin
 toggleResult = Set.fromList [(Point 0 1), (Point 1 1), (Point 1 0)]
 
 
-tests = test [-- "turn on 0,0 through 999,999" ~: 1000000 ~=? run "turn on 0,0 through 999,999"
-              "exec turn on 0-1,0-2" ~: 6 ~=? Set.size (execute Set.empty On (XRange 0 1) (YRange 0 2))
-           ,  "exec turn off 0-0,1-1" ~: setOfOrigin ~=? execute setOfTwo Off (XRange 0 0) (YRange 1 1)
-           ,  "exec toggle 0-0,1-1" ~: toggleResult ~=? execute setOfOrigin Toggle (XRange 0 1) (YRange 0 1)
+tests = test ["run several commands" ~: 999999 ~=? runScript [
+                "turn on 0,0 through 999,999"
+              , "turn off 0,0 through 0,0"]
+             , "parse on" ~: (Cmd On (XRange 887 959) (YRange 9 629)) ~=? parseCmd "turn on 887,9 through 959,629" 
+             , "parse toggle" ~: (Cmd Toggle (XRange 887 959) (YRange 9 629)) ~=? parseCmd "toggle 887,9 through 959,629" 
+             , "exec turn on 0-1,0-2" ~: 6 ~=? Set.size (execute Set.empty (Cmd On (XRange 0 1) (YRange 0 2)))
+             , "exec turn off 0-0,1-1" ~: setOfOrigin ~=? execute setOfTwo (Cmd Off (XRange 0 0) (YRange 1 1))
+             , "exec toggle 0-0,1-1" ~: toggleResult ~=? execute setOfOrigin (Cmd Toggle (XRange 0 1) (YRange 0 1))
              ]
 
 run = runTestTT tests
