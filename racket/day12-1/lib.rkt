49c826987d0900cb70431971c38ffeb918fd53f6
Steve Gravrock <sdg@panix.com>
2017-12-27 19:47:48 -0800

    Day 12 part 1 (Racket): working

--- racket/day12-1/lib.rkt
+++ racket/day12-1/lib.rkt
@@ -8,4 +8,54 @@
     "")
    " "))
 
-(provide parse-line)
+(define (any? predicate list)
+  (not (equal? #f (findf predicate list))))
+
+(define (contains? needle haystack)
+  (not (equal? #f (member needle haystack))))
+
+(define (debug-if result debug)
+  (cond [result (debug)])
+  result)
+
+(define (all-that-reach dest-id lines)
+  (let ([src-ids (map car lines)])
+    (printf "src IDs: ~a\n" src-ids)
+    (filter (lambda (src-id) (reaches src-id dest-id lines)) src-ids)))
+
+(define (reaches src-id dest-id lines)
+  (or (equal? src-id dest-id)
+      (any? (lambda (line)
+              (debug-if
+               (reaches-via line src-id dest-id lines)
+               (lambda ()
+                 (printf "Top: ~a is reachable from ~a via line ~a\n" dest-id src-id line))))
+            lines)))
+
+(define (reaches-directly line dest-id)
+  (debug-if (equal? dest-id (car line))
+            (lambda () (printf "~a is directly reachable via line ~a\n" dest-id line))))
+
+(define (reaches-via start-line src-id dest-id lines)
+  (and (line-has-source? start-line src-id)
+       (let ([candidate-lines (remq start-line lines)])
+         (or (reaches-directly start-line dest-id)
+             (debug-if
+              (reaches-indirectly (car start-line) dest-id candidate-lines)
+              (lambda ()
+                (printf "~a is indirectly reachable from ~a" dest-id (car start-line))))))))
+
+(define (reaches-indirectly src-id dest-id lines)
+  (any? (lambda (line)
+          (reaches-via line src-id dest-id lines))
+        lines))
+
+(define (line-has-source? line source-id)
+  (not (equal? #f (member source-id (cdr line)))))
+
+(define (find-line id lines)
+  (findf (lambda (line)
+           (equal? id (car line)))
+         lines))
+
+(provide parse-line reaches reaches-directly reaches-via find-line debug-if all-that-reach)
--- racket/day12-1/tests.rkt
+++ racket/day12-1/tests.rkt
@@ -5,3 +5,39 @@
 
 (check-equal? (parse-line "0 <-> 2") '("0" "2"))
 (check-equal? (parse-line "2 <-> 0, 3, 4") '("2" "0" "3" "4"))
+
+
+(let ([sample-input '(("0" "2")
+                      ("1" "1")
+                      ("2" "0" "3" "4")
+                      ("3" "2" "4")
+                      ("4" "2" "3" "6")
+                      ("5" "6")
+                      ("6" "4" "5")
+                      ("7" "1")
+                     )])
+
+  (let ([called #f])
+    (check-false (debug-if #f (lambda () (set! called #t))))
+    (check-false called))
+  (let ([called #f])
+    (check-true (debug-if #t (lambda () (set! called #t))))
+    (check-true called))
+  
+  (check-equal? (find-line "5" sample-input) '("5" "6"))
+  
+  (check-true (reaches-directly '("4" "2") "4"))
+  (check-false (reaches-directly '("4" "2") "3"))
+  
+  (check-false (reaches-via '("1" "1") "1" "0" sample-input))
+  (check-true  (reaches-via '("0" "1") "1" "0" sample-input))
+  (check-true (reaches-via '("2" "0" "3" "4") "4" "0" sample-input))
+  (check-false (reaches-via '("2" "0" "3" "4") "5" "0" sample-input))
+  (check-false (reaches-via '("7" "1") "1" "0" sample-input))
+
+  (check-true (reaches "0" "0" sample-input))
+  (check-true (reaches "2" "0" sample-input))
+  (check-false (reaches "1" "0" sample-input))
+
+  (check-equal? (all-that-reach "0" sample-input) '("0" "2" "3" "4" "5" "6"))
+)
