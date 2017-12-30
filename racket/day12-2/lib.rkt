d11445f51c50b642dbaa0a53cc2c9aaa899a13e8
Steve Gravrock <sdg@panix.com>
2017-12-30 12:11:54 -0800

    Day 12 part 2 (Racket): removed debug logging
    Now completes instantly rather than taking until the heat death of the universe

--- racket/day12-2/lib.rkt
+++ racket/day12-2/lib.rkt
@@ -18,25 +18,18 @@
   (find-groups2 lines '()))
 
 (define (find-groups2 lines groups)
-  (printf "find-groups2 ~a ~a\n" lines groups)
   (if (equal? 0 (length lines))
       groups
       (find-groups2 (cdr lines) (add-to-groups (car lines) groups))))
 
 (define (add-to-groups line groups)
-  (printf "add-to-groups ~a ~a\n" line groups)
   (let-values ([(matches non-matches)
                 (partition (lambda (group) (line-touches-group? line group)) groups)])
     (let* ([matches-with-line (cons line matches)]
-           [merged-matches (set-union-all matches-with-line)]
-           [result (append (list merged-matches) non-matches)])
-      (printf "matches-with-line: ~a\n" matches-with-line)
-      (printf "merged-matches: ~a\n" merged-matches)
-      (printf "result: ~a\n" result)
-      result)))
+           [merged-matches (set-union-all matches-with-line)])
+      (append (list merged-matches) non-matches))))
 
 (define (line-touches-group? line group)
-  (printf "line-touches-group ~a ~a\n" line group)
   (any? (lambda (id) (contains? id group)) line))
 
 (define (set-union-all sets)
