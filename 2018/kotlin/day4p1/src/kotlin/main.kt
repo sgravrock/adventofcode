347dca044a4d2f77072cc1213f5323d78c5d5cd5
Steve Gravrock <sdg@panix.com>
2018-12-09 17:20:07 -0800

    Day 4 part 1 (Kotlin): working

--- 2018/kotlin/day4p1/src/kotlin/main.kt
+++ 2018/kotlin/day4p1/src/kotlin/main.kt
@@ -1,57 +1,46 @@
-import java.io.File
 import java.lang.Exception
 
 fun main(args: Array<String>) {
-    val classLoader = ClassForFindingClassloader::class.java.classLoader
+    val classLoader = Time::class.java.classLoader
     val input = classLoader.getResource("input.txt").readText()
     println(puzzleSolution(parseInput(input)))
 }
 
-class ClassForFindingClassloader {}
-
-data class Time(val hour: Int, val minute: Int)
-data class TimeRange(val start: Time, val end: Time)
-data class Shift(val guard: Int, val sleeps: List<TimeRange>)
-
-class ShiftsBuilder {
-    val shifts = mutableListOf<Shift>()
-    var curGuard: Int? = null
-    var curFinishedSleeps = mutableListOf<TimeRange>()
-    var curSleepStart: Time? = null
-
-    fun startShift(guard: Int) {
-        finalizeShift()
-        curGuard = guard
-    }
-
-    fun finalizeShift() {
-        if (curGuard == null) {
-            return
-        }
-
-        if (curSleepStart != null) {
-            throw Exception("Can't finalize a shift mid-sleep")
-        }
-
-        shifts.add(Shift(curGuard!!, curFinishedSleeps))
-        curGuard = null
-        curFinishedSleeps = mutableListOf()
-        curSleepStart = null
-    }
-
-    fun startSleep(time: Time) {
-        curSleepStart = time
-    }
-
-    fun endSleep(time: Time) {
-        curFinishedSleeps.add(TimeRange(curSleepStart!!, time))
-        curSleepStart = null
-    }
+data class Time(val hour: Int, val minute: Int) {
+    fun toMinutes() = hour * 60 + minute
 }
 
-fun parseInput(input: String): List<Shift> {
-    val builder = ShiftsBuilder()
+data class TimeRange(val start: Time, val end: Time)
 
+fun sleepiestGuard(sleepsByGuard: Map<Int, List<TimeRange>>): Int {
+    return sleepsByGuard.entries
+            .sortedBy { totalMinutes(it.value) }
+            .last().key
+}
+
+fun sleepiestMinute(sleeps: List<TimeRange>): Int {
+    return (0..59)
+            .sortedBy { minute ->
+                sleeps.filter {
+                    it.start.hour == 0 &&
+                            it.end.hour == 0 &&
+                            it.start.minute <= minute &&
+                            it.end.minute > minute
+                }.count()
+            }
+            .last()
+}
+
+fun totalMinutes(ranges: List<TimeRange>): Int {
+    // Offer void where prohibited or if any sleep spans midnight.
+    // Do not fold, spindle, or mutilate.
+    return ranges.map { it.end.toMinutes() - it.start.toMinutes() }.sum()
+}
+
+fun parseInput(input: String): Map<Int, List<TimeRange>> {
+    val result = mutableMapOf<Int, MutableList<TimeRange>>()
+    var curGuard: Int? = null
+    var curSleepStart: Time? = null
     val shiftStartRe = Regex("Guard #([\\d]+) begins shift")
     val sleepStartRe = Regex("([\\d]{2}):([\\d]{2})\\] falls asleep")
     val sleepEndRe = Regex("([\\d]{2}):([\\d]{2})\\] wakes up")
@@ -62,26 +51,32 @@ fun parseInput(input: String): List<Shift> {
         val sleepEndMatch = sleepEndRe.find(line)
 
         if (shiftMatch != null) {
-            builder.startShift(shiftMatch.groupValues[1].toInt())
+            curGuard = shiftMatch.groupValues[1].toInt()
+            if (!result.containsKey(curGuard)) {
+                result[curGuard] = mutableListOf()
+            }
         } else if (sleepStartMatch != null) {
-            builder.startSleep(Time(
+            curSleepStart = Time(
                     sleepStartMatch.groupValues[1].toInt(),
                     sleepStartMatch.groupValues[2].toInt()
-            ))
+            )
         } else if (sleepEndMatch != null) {
-            builder.endSleep(Time(
-                    sleepEndMatch.groupValues[1].toInt(),
-                    sleepEndMatch.groupValues[2].toInt()
+            result[curGuard]!!.add(TimeRange(
+                    curSleepStart!!,
+                    Time(
+                            sleepEndMatch.groupValues[1].toInt(),
+                            sleepEndMatch.groupValues[2].toInt()
+                    )
             ))
         } else {
             throw Exception("Parse error: ${line}")
         }
     }
 
-    builder.finalizeShift()
-    return builder.shifts
+    return result
 }
 
-fun puzzleSolution(shifts: List<Shift>): Int {
-    return -1
+fun puzzleSolution(sleepsByGuard: Map<Int, List<TimeRange>>): Int {
+    val guard = sleepiestGuard(sleepsByGuard)
+    return guard * sleepiestMinute(sleepsByGuard[guard]!!)
 }
--- 2018/kotlin/day4p1/test/Tests.kt
+++ 2018/kotlin/day4p1/test/Tests.kt
@@ -21,31 +21,44 @@ class Tests {
                 "[1518-11-05 00:03] Guard #99 begins shift\n" +
                 "[1518-11-05 00:45] falls asleep\n" +
                 "[1518-11-05 00:55] wakes up"
-        val expected = listOf(
-                Shift(
-                        guard = 10,
-                        sleeps = listOf(
-                                TimeRange(Time(0, 5), Time(0, 25)),
-                                TimeRange(Time(0, 30), Time(0, 55))
-                        )
+        val expected = mapOf(
+                10 to listOf(
+                        TimeRange(Time(0, 5), Time(0, 25)),
+                        TimeRange(Time(0, 30), Time(0, 55)),
+                        TimeRange(Time(0, 24), Time(0, 29))
                 ),
-                Shift(
-                        guard = 99,
-                        sleeps = listOf(TimeRange(Time(0, 40), Time(0, 50)))
-                ),
-                Shift(
-                        guard = 10,
-                        sleeps = listOf(TimeRange(Time(0, 24), Time(0, 29)))
-                ),
-                Shift(
-                        guard = 99,
-                        sleeps = listOf(TimeRange(Time(0, 36), Time(0, 46)))
-                ),
-                Shift(
-                        guard = 99,
-                        sleeps = listOf(TimeRange(Time(0, 45), Time(0, 55)))
+                99 to listOf(
+                        TimeRange(Time(0, 40), Time(0, 50)),
+                        TimeRange(Time(0, 36), Time(0, 46)),
+                        TimeRange(Time(0, 45), Time(0, 55))
                 )
         )
         assertEquals(expected, parseInput(input))
+    }
+
+    @Test
+    fun testSleepiestGuard() {
+        val input = mapOf(
+                10 to listOf(
+                        TimeRange(Time(0, 5), Time(0, 25)),
+                        TimeRange(Time(0, 30), Time(0, 55)),
+                        TimeRange(Time(0, 24), Time(0, 29))
+                ),
+                99 to listOf(
+                        TimeRange(Time(0, 0), Time(0, 50)),
+                        TimeRange(Time(0, 24), Time(0, 29))
+                )
+        )
+        assertEquals(99, sleepiestGuard(input))
+    }
+
+    @Test
+    fun testSleepiestMinute() {
+        val input = listOf(
+                TimeRange(Time(0, 5), Time(0, 25)),
+                TimeRange(Time(0, 30), Time(0, 55)),
+                TimeRange(Time(0, 24), Time(0, 29))
+        )
+        assertEquals(24, sleepiestMinute(input))
     }
 }
