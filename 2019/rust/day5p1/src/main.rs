0069d6b17a2f7300c1123704d5d6a18db0eb9f9f
Steve Gravrock <sdg@panix.com>
2019-12-04 21:41:37 -0800

    Day 5 part 1 (rust): input and output instructions

--- 2019/rust/day5p1/src/main.rs
+++ 2019/rust/day5p1/src/main.rs
@@ -20,34 +20,59 @@ fn test_parse_program() {
 	assert_eq!(parse_program("5,0,99,2,14"), vec![5,0,99,2,14]);
 }
 
-fn execute(program: &mut Vec<usize>) {
+fn execute(program: &mut Vec<usize>, input: Option<usize>) -> Vec<usize> {
 	let mut ip: usize = 0;
+	let mut output: Vec<usize> = vec![];
 
 	while program[ip] != 99 {
 		let opcode = program[ip];
 
-		if opcode != 1 && opcode != 2 {
+		if opcode == 1  || opcode == 2 {
+			let arg0 = program[program[ip + 1]];
+			let arg1 = program[program[ip + 2]];
+			let dest = program[ip + 3];
+	
+			if opcode == 1 {
+				program[dest] = arg0 + arg1;
+			} else {
+				program[dest] = arg0 * arg1;
+			}
+	
+			ip += 4;
+		} else if opcode == 3 {
+			let dest = program[ip + 1];
+			program[dest] = input.unwrap();
+			ip += 2;
+		} else if opcode == 4 {
+			output.push(program[program[ip + 1]]);
+			ip += 2;
+		} else {
 			panic!("Unrecognized opcode {} at ip={}", program[ip], ip);
 		}
 
-		let arg0 = program[program[ip + 1]];
-		let arg1 = program[program[ip + 2]];
-		let dest = program[ip + 3];
-
-		if opcode == 1 {
-			program[dest] = arg0 + arg1;
-		} else {
-			program[dest] = arg0 * arg1;
-		}
-
-		ip += 4;
 	}
+
+	output
 }
 
 #[test]
 fn test_execute() {
 	let mut program = vec![1,9,10,3,2,3,11,0,99,30,40,50];
 	let expected = vec![3500,9,10,70,2,3,11,0,99,30,40,50];
-	execute(&mut program);
+	execute(&mut program, None);
 	assert_eq!(program, expected);
+}
+
+#[test]
+fn test_execute_input() {
+	let mut program = vec![3,4,99,0,0];
+	let expected = vec![3,4,99,0,5];
+	execute(&mut program, Some(5));
+	assert_eq!(program, expected);
+}
+
+#[test]
+fn test_execute_output() {
+	let mut program = vec![4,3,99,12345];
+	assert_eq!(execute(&mut program, None), vec![12345]);
 }
