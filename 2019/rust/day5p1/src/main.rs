3ec71124db87f6d2e28b4e1f93d9f45737150962
Steve Gravrock <sdg@panix.com>
2019-12-04 22:26:40 -0800

    Day 5 part 1 (rust): immediate mode

--- 2019/rust/day5p1/src/main.rs
+++ 2019/rust/day5p1/src/main.rs
@@ -8,6 +8,56 @@ struct Inputs {
 	verb: i32,
 }
 
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum Mode {
+	Immed,
+	Pos
+}
+
+#[derive(Debug, PartialEq)]
+struct Instruction {
+	opcode: i32,
+	param_modes: Vec<Mode>
+}
+
+impl Instruction {
+	fn decode(mut word: i32) -> Instruction {
+		let opcode = word % 100;
+		word /= 100;
+		let mut param_modes: Vec<Mode> = vec![];
+
+		while word > 0 {
+			param_modes.insert(0, match word % 10 {
+				0 => Mode::Pos,
+				1 => Mode::Immed,
+				_ => panic!("Unrecognized parameter mode {}", word % 10)
+			});
+			word /= 10;
+		}
+
+		Instruction {opcode, param_modes}
+	}
+
+	fn param_mode(&self, i: usize) -> Mode {
+		match self.param_modes.get(i) {
+			Some(x) => *x,
+			None => Mode::Pos
+		}
+	}
+}
+
+#[test]
+fn test_instruction_decode() {
+	assert_eq!(
+		Instruction::decode(1002),
+		Instruction {opcode: 2, param_modes: vec![Mode::Immed, Mode::Pos]}
+	);
+	assert_eq!(
+		Instruction::decode(2),
+		Instruction {opcode: 2, param_modes: vec![]}
+	);
+}
+
 fn parse_program(input: &str) -> Vec<i32> {
 	input
 		.split(",")
@@ -25,26 +75,27 @@ fn execute(program: &mut Vec<i32>, input: Option<i32>) -> Vec<i32> {
 	let mut output: Vec<i32> = vec![];
 
 	while program[ip] != 99 {
-		let opcode = program[ip];
+		let instruction = Instruction::decode(program[ip]);
 
-		if opcode == 1  || opcode == 2 {
-			let arg0 = program[program[ip as usize + 1] as usize];
-			let arg1 = program[program[ip as usize + 2] as usize];
+		if instruction.opcode == 1  || instruction.opcode == 2 {
+			let arg0 = lvalue(program, &instruction, ip, 0);
+			let arg1 = lvalue(program, &instruction, ip, 1);
 			let dest = program[ip + 3] as usize;
 	
-			if opcode == 1 {
+			if instruction.opcode == 1 {
 				program[dest] = arg0 + arg1;
 			} else {
 				program[dest] = arg0 * arg1;
 			}
 	
 			ip += 4;
-		} else if opcode == 3 {
+		} else if instruction.opcode == 3 {
 			let dest = program[ip + 1] as usize;
 			program[dest] = input.unwrap();
 			ip += 2;
-		} else if opcode == 4 {
-			output.push(program[program[ip + 1] as usize]);
+		} else if instruction.opcode == 4 {
+			let v = lvalue(program, &instruction, ip, 0);
+			output.push(v);
 			ip += 2;
 		} else {
 			panic!("Unrecognized opcode {} at ip={}", program[ip], ip);
@@ -53,6 +104,14 @@ fn execute(program: &mut Vec<i32>, input: Option<i32>) -> Vec<i32> {
 	}
 
 	output
+}
+
+fn lvalue(program: &Vec<i32>, instruction: &Instruction, ip: usize, param_ix: usize) -> i32 {
+	let x = program[ip + param_ix + 1];
+	match instruction.param_mode(param_ix as usize) {
+		Mode::Immed => x,
+		Mode::Pos => program[x as usize]
+	}
 }
 
 #[test]
@@ -83,4 +142,12 @@ fn test_execute_input() {
 fn test_execute_output() {
 	let mut program = vec![4,3,99,12345];
 	assert_eq!(execute(&mut program, None), vec![12345]);
+}
+
+#[test]
+fn test_execute_immediate_mode() {
+	let mut program = vec![1102,4,3,5,99,0];
+	let expected = vec![1102,4,3,5,99,12];
+	execute(&mut program, Some(5));
+	assert_eq!(program, expected);
 }
