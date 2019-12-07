2842c380d5d579ae0369c46b718e0dfa22fd1077
Steve Gravrock <sdg@panix.com>
2019-12-07 09:32:05 -0800

    Day 5 part 2 (rust): Refactor to support breaking on errors
    This is to set the stage for automatically launching the debugger when
    errors like out of bounds access occur. Ideally we'd "just" catch the
    panic, like so:
    
    	match panic::catch_unwind(|| { machine.execute() }) {
    		Ok(_) => (),
    		Err(_) => debug(&mut machine)
    	}
    
    But it's not possible to borrow mutably across the catch_unwind
    boundary:
    
    	error[E0277]: the type `&mut Machine` may not be safely transferred across an unwind boundary
    	   --> src/main.rs:13:8
    	    |
    	13  |     match panic::catch_unwind(|| { machine.execute() }) {
    	    |           ^^^^^^^^^^^^^^^^^^^ `&mut Machine` may not be safely transferred across an unwind boundary
    
    So we'll have to propagate results up all the way to the top instead of
    allowing panics to happen. This moves things in that direction by running
    all of the argument reads through a single point in do_current_instruction.
    
    Also, something in this makes part 2 work correctly.

--- 2019/rust/day5p2/src/main.rs
+++ 2019/rust/day5p2/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 		"3,225,1,225,6,6,1100,1,238,225,104,0,1102,78,40,225,1102,52,43,224,1001,224,-2236,224,4,224,102,8,223,223,101,4,224,224,1,224,223,223,1,191,61,224,1001,224,-131,224,4,224,102,8,223,223,101,4,224,224,1,223,224,223,1101,86,74,225,1102,14,76,225,1101,73,83,224,101,-156,224,224,4,224,102,8,223,223,101,6,224,224,1,224,223,223,1102,43,82,225,2,196,13,224,101,-6162,224,224,4,224,102,8,223,223,101,5,224,224,1,223,224,223,1001,161,51,224,101,-70,224,224,4,224,102,8,223,223,1001,224,1,224,1,224,223,223,102,52,187,224,1001,224,-832,224,4,224,102,8,223,223,101,1,224,224,1,224,223,223,1102,19,79,225,101,65,92,224,1001,224,-147,224,4,224,1002,223,8,223,101,4,224,224,1,223,224,223,1102,16,90,225,1102,45,44,225,1102,92,79,225,1002,65,34,224,101,-476,224,224,4,224,102,8,223,223,1001,224,5,224,1,224,223,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,107,226,226,224,1002,223,2,223,1005,224,329,1001,223,1,223,1007,226,226,224,102,2,223,223,1005,224,344,101,1,223,223,1008,226,226,224,102,2,223,223,1005,224,359,1001,223,1,223,8,226,677,224,102,2,223,223,1006,224,374,101,1,223,223,1107,226,677,224,1002,223,2,223,1006,224,389,101,1,223,223,1108,226,677,224,102,2,223,223,1005,224,404,101,1,223,223,107,677,677,224,102,2,223,223,1006,224,419,1001,223,1,223,7,677,226,224,102,2,223,223,1005,224,434,101,1,223,223,1007,677,677,224,102,2,223,223,1005,224,449,1001,223,1,223,108,226,677,224,102,2,223,223,1005,224,464,1001,223,1,223,108,226,226,224,102,2,223,223,1006,224,479,101,1,223,223,107,226,677,224,102,2,223,223,1006,224,494,1001,223,1,223,7,226,226,224,1002,223,2,223,1006,224,509,101,1,223,223,1108,677,226,224,102,2,223,223,1005,224,524,101,1,223,223,1107,677,226,224,102,2,223,223,1005,224,539,101,1,223,223,1008,677,226,224,102,2,223,223,1005,224,554,101,1,223,223,1008,677,677,224,1002,223,2,223,1006,224,569,101,1,223,223,1107,677,677,224,102,2,223,223,1006,224,584,1001,223,1,223,1108,226,226,224,1002,223,2,223,1006,224,599,101,1,223,223,7,226,677,224,102,2,223,223,1006,224,614,101,1,223,223,108,677,677,224,1002,223,2,223,1006,224,629,101,1,223,223,1007,677,226,224,102,2,223,223,1006,224,644,101,1,223,223,8,677,677,224,1002,223,2,223,1006,224,659,101,1,223,223,8,677,226,224,102,2,223,223,1005,224,674,101,1,223,223,4,223,99,226";
 	let mut machine = Machine::new(parse_program(input));
 	machine.input = Some(5);
-	debug(&mut machine);
+	machine.execute();
 	println!("Output: {:?}", machine.output);
 }
 
@@ -88,6 +88,12 @@ fn test_parse_program() {
 	assert_eq!(parse_program("5,0,99,2,14"), vec![5,0,99,2,14]);
 }
 
+struct ParamValues {
+	arg0: Option<i32>,
+	arg1: Option<i32>,
+	dest: Option<usize>
+}
+
 struct Machine {
 	mem: Vec<i32>,
 	input: Option<i32>,
@@ -112,59 +118,100 @@ impl Machine {
 
 	fn do_current_instruction(&mut self) {
 		let instruction = self.current_instruction();
+		let params = self.evaluate_params(&instruction);
 
-		if instruction.opcode == 1  || instruction.opcode == 2 {
-			let arg0 = self.lvalue(&instruction, 0);
-			let arg1 = self.lvalue(&instruction, 1);
-			let dest = self.mem[self.ip + 3] as usize;
-	
-			if instruction.opcode == 1 {
-				self.mem[dest] = arg0 + arg1;
-			} else {
-				self.mem[dest] = arg0 * arg1;
+		match instruction.opcode {
+			1 => {
+				self.mem[params.dest.unwrap()] =
+					params.arg0.unwrap() + params.arg1.unwrap();
+				self.ip += 4;
+			},
+			2 => {
+				self.mem[params.dest.unwrap()] =
+					params.arg0.unwrap() * params.arg1.unwrap();
+				self.ip += 4;
+			},
+			3 => {
+				// TODO: Propagate errors from self.input.unwrap()
+				self.mem[params.dest.unwrap()] = self.input.unwrap();
+				self.ip += 2;
+			},
+			4 => {
+				self.output.push(params.arg0.unwrap());
+				self.ip += 2;
+			},
+			5 => {
+				if params.arg0.unwrap() == 0 {
+					self.ip += 3;
+				} else {
+					// TODO: detect & propagate range errors
+					self.ip = params.arg1.unwrap() as usize;
+				}
+			},
+			6 => {
+				if params.arg0.unwrap() == 0 {
+					// TODO: detect & propagate range errors
+					self.ip = params.arg1.unwrap() as usize;
+				} else {
+					self.ip += 3;
+				}
+			},
+			7 => {
+				self.mem[params.dest.unwrap()] = 
+					if params.arg0.unwrap() < params.arg1.unwrap() {
+						1
+					} else {
+						0
+					};
+				self.ip += 4;
+			},
+			8 => {
+				self.mem[params.dest.unwrap()] = 
+					if params.arg0.unwrap() == params.arg1.unwrap() {
+						1
+					} else {
+						0
+					};
+				self.ip += 4;
+			},
+			_ => {
+				panic!("Unrecognized opcode {} at self.ip={}",
+					self.mem[self.ip], self.ip)
 			}
-	
-			self.ip += 4;
-		} else if instruction.opcode == 3 {
-			let dest = self.mem[self.ip + 1] as usize;
-			self.mem[dest] = self.input.unwrap();
-			self.ip += 2;
-		} else if instruction.opcode == 4 {
-			let v = self.lvalue(&instruction, 0);
-			self.output.push(v);
-			self.ip += 2;
-		} else if instruction.opcode == 5 {
-			let cond = self.lvalue(&instruction, 0);
-			if cond == 0 {
-				self.ip += 3;
-			} else {
-				self.ip = self.lvalue(&instruction, 1) as usize;
+		}
+	}
+
+	fn evaluate_params(&self, instruction: &Instruction) -> ParamValues {
+		// TODO: detect & propagate range errors in dest
+		match instruction.opcode {
+			1 | 2 | 7 | 8 => ParamValues {
+				arg0: Some(self.lvalue(&instruction, 0)),
+				arg1: Some(self.lvalue(&instruction, 1)),
+				dest: Some(self.mem[self.ip + 3] as usize),
+			},
+			3 => ParamValues {
+				arg0: None,
+				arg1: None,
+				dest: Some(self.mem[self.ip + 1] as usize),
+			},
+			4 => ParamValues {
+				arg0: Some(self.lvalue(&instruction, 0)),
+				arg1: None,
+				dest: None
+			},
+			5 | 6 => ParamValues {
+				arg0: Some(self.lvalue(&instruction, 0)),
+				arg1: Some(self.lvalue(&instruction, 1)),
+				dest: None
+			},
+			_ => {
+				panic!("Unrecognized opcode {} at self.ip={}", self.mem[self.ip], self.ip);
 			}
-		} else if instruction.opcode == 6 {
-			let cond = self.lvalue(&instruction, 0);
-			if cond == 0 {
-				self.ip = self.lvalue(&instruction, 1) as usize;
-			} else {
-				self.ip += 3;
-			}
-		} else if instruction.opcode == 7 {
-			let a = self.lvalue(&instruction, 0);
-			let b = self.lvalue(&instruction, 1);
-			let dest = self.mem[self.ip + 3] as usize;
-			self.mem[dest] = if a < b { 1 } else { 0 };
-			self.ip = 4;
-		} else if instruction.opcode == 8 {
-			let a = self.lvalue(&instruction, 0);
-			let b = self.lvalue(&instruction, 1);
-			let dest = self.mem[self.ip + 3] as usize;
-			self.mem[dest] = if a == b { 1 } else { 0 };
-			self.ip = 4;
-		} else {
-			panic!("Unrecognized opcode {} at self.ip={}", self.mem[self.ip], self.ip);
 		}
 	}
 
 	fn lvalue(&self, instruction: &Instruction, param_ix: usize) -> i32 {
+		// TODO: detect & propagate bounds errors
 		let x = self.mem[self.ip + param_ix + 1];
 		match instruction.param_mode(param_ix) {
 			Mode::Immed => x,
