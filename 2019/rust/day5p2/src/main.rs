32732755b76dd0e0e9450b31f7b70f56a0ebca65
Steve Gravrock <sdg@panix.com>
2019-12-07 11:55:05 -0800

    Day 5 part 2 (rust): Break into the debugger on most errors

--- 2019/rust/day5p2/src/main.rs
+++ 2019/rust/day5p2/src/main.rs
@@ -2,20 +2,28 @@ use std::io;
 use std::io::Write;
 #[macro_use] extern crate text_io;
 use std::cmp::min;
+use std::convert::TryFrom;
 
 fn main() {
 	let input = 
 		"3,225,1,225,6,6,1100,1,238,225,104,0,1102,78,40,225,1102,52,43,224,1001,224,-2236,224,4,224,102,8,223,223,101,4,224,224,1,224,223,223,1,191,61,224,1001,224,-131,224,4,224,102,8,223,223,101,4,224,224,1,223,224,223,1101,86,74,225,1102,14,76,225,1101,73,83,224,101,-156,224,224,4,224,102,8,223,223,101,6,224,224,1,224,223,223,1102,43,82,225,2,196,13,224,101,-6162,224,224,4,224,102,8,223,223,101,5,224,224,1,223,224,223,1001,161,51,224,101,-70,224,224,4,224,102,8,223,223,1001,224,1,224,1,224,223,223,102,52,187,224,1001,224,-832,224,4,224,102,8,223,223,101,1,224,224,1,224,223,223,1102,19,79,225,101,65,92,224,1001,224,-147,224,4,224,1002,223,8,223,101,4,224,224,1,223,224,223,1102,16,90,225,1102,45,44,225,1102,92,79,225,1002,65,34,224,101,-476,224,224,4,224,102,8,223,223,1001,224,5,224,1,224,223,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,107,226,226,224,1002,223,2,223,1005,224,329,1001,223,1,223,1007,226,226,224,102,2,223,223,1005,224,344,101,1,223,223,1008,226,226,224,102,2,223,223,1005,224,359,1001,223,1,223,8,226,677,224,102,2,223,223,1006,224,374,101,1,223,223,1107,226,677,224,1002,223,2,223,1006,224,389,101,1,223,223,1108,226,677,224,102,2,223,223,1005,224,404,101,1,223,223,107,677,677,224,102,2,223,223,1006,224,419,1001,223,1,223,7,677,226,224,102,2,223,223,1005,224,434,101,1,223,223,1007,677,677,224,102,2,223,223,1005,224,449,1001,223,1,223,108,226,677,224,102,2,223,223,1005,224,464,1001,223,1,223,108,226,226,224,102,2,223,223,1006,224,479,101,1,223,223,107,226,677,224,102,2,223,223,1006,224,494,1001,223,1,223,7,226,226,224,1002,223,2,223,1006,224,509,101,1,223,223,1108,677,226,224,102,2,223,223,1005,224,524,101,1,223,223,1107,677,226,224,102,2,223,223,1005,224,539,101,1,223,223,1008,677,226,224,102,2,223,223,1005,224,554,101,1,223,223,1008,677,677,224,1002,223,2,223,1006,224,569,101,1,223,223,1107,677,677,224,102,2,223,223,1006,224,584,1001,223,1,223,1108,226,226,224,1002,223,2,223,1006,224,599,101,1,223,223,7,226,677,224,102,2,223,223,1006,224,614,101,1,223,223,108,677,677,224,1002,223,2,223,1006,224,629,101,1,223,223,1007,677,226,224,102,2,223,223,1006,224,644,101,1,223,223,8,677,677,224,1002,223,2,223,1006,224,659,101,1,223,223,8,677,226,224,102,2,223,223,1005,224,674,101,1,223,223,4,223,99,226";
 	let mut machine = Machine::new(parse_program(input));
 	machine.input = Some(5);
-	machine.execute();
-	println!("Output: {:?}", machine.output);
+	match machine.execute() {
+		Ok(_) => println!("Output: {:?}", machine.output),
+		Err(error) => {
+			println!("Execution failed: {:?}", error);
+			debug(&mut machine);
+		}
+	}
 }
 
 #[derive(Debug, PartialEq, Copy, Clone)]
-struct Inputs {
-	noun: i32,
-	verb: i32,
+enum Error {
+	OutOfBoundsRead { index: i32 },
+	OutOfBoundsWrite { index: i32 },
+	InvalidOpcode { opcode: i32, ip: i32 },
+	IpOutOfRange { ip: i32 },
 }
 
 #[derive(Debug, PartialEq, Copy, Clone)]
@@ -91,14 +99,14 @@ fn test_parse_program() {
 struct ParamValues {
 	arg0: Option<i32>,
 	arg1: Option<i32>,
-	dest: Option<usize>
+	dest: Option<i32>
 }
 
 struct Machine {
 	mem: Vec<i32>,
 	input: Option<i32>,
 	output: Vec<i32>,
-	ip: usize,
+	ip: i32,
 }
 
 impl Machine {
@@ -106,34 +114,42 @@ impl Machine {
 		Machine { mem, input: None, output: vec![], ip: 0, }
 	}
 
-	fn execute(&mut self) {
-		while self.mem[self.ip] != 99 {
-			self.do_current_instruction();
+	fn execute(&mut self) -> Result<(), Error> {
+		while self.read_instruction()? != 99 {
+			self.do_current_instruction()?;
 		}
+		Ok(())
 	}
 
-	fn current_instruction(&self) -> Instruction {
-		Instruction::decode(self.mem[self.ip])
+	fn current_instruction(&self) -> Result<Instruction, Error> {
+		self.read_instruction().and_then(|word| {
+			Ok(Instruction::decode(word))
+		})
 	}
 
-	fn do_current_instruction(&mut self) {
-		let instruction = self.current_instruction();
-		let params = self.evaluate_params(&instruction);
+	fn read_instruction(&self) -> Result<i32, Error> {
+		self.checked_read(self.ip as i32).or_else(|_| {
+			Err(Error::IpOutOfRange {ip: self.ip as i32})
+		})
+	}
+
+	fn do_current_instruction(&mut self) -> Result<(), Error> {
+		let instruction = self.current_instruction()?;
+		let params = self.evaluate_params(&instruction)?;
+		let mut rvalue: Option<i32> = None;
 
 		match instruction.opcode {
 			1 => {
-				self.mem[params.dest.unwrap()] =
-					params.arg0.unwrap() + params.arg1.unwrap();
+				rvalue = Some(params.arg0.unwrap() + params.arg1.unwrap());
 				self.ip += 4;
 			},
 			2 => {
-				self.mem[params.dest.unwrap()] =
-					params.arg0.unwrap() * params.arg1.unwrap();
+				rvalue = Some(params.arg0.unwrap() * params.arg1.unwrap());
 				self.ip += 4;
 			},
 			3 => {
 				// TODO: Propagate errors from self.input.unwrap()
-				self.mem[params.dest.unwrap()] = self.input.unwrap();
+				rvalue = Some(self.input.unwrap());
 				self.ip += 2;
 			},
 			4 => {
@@ -144,78 +160,103 @@ impl Machine {
 				if params.arg0.unwrap() == 0 {
 					self.ip += 3;
 				} else {
-					// TODO: detect & propagate range errors
-					self.ip = params.arg1.unwrap() as usize;
+					self.ip = params.arg1.unwrap();
 				}
 			},
 			6 => {
 				if params.arg0.unwrap() == 0 {
-					// TODO: detect & propagate range errors
-					self.ip = params.arg1.unwrap() as usize;
+					self.ip = params.arg1.unwrap();
 				} else {
 					self.ip += 3;
 				}
 			},
 			7 => {
-				self.mem[params.dest.unwrap()] = 
+				rvalue = Some(
 					if params.arg0.unwrap() < params.arg1.unwrap() {
 						1
 					} else {
 						0
-					};
+					}
+				);
 				self.ip += 4;
 			},
 			8 => {
-				self.mem[params.dest.unwrap()] = 
+				rvalue = Some(
 					if params.arg0.unwrap() == params.arg1.unwrap() {
 						1
 					} else {
 						0
-					};
+					}
+				);
 				self.ip += 4;
 			},
 			_ => {
-				panic!("Unrecognized opcode {} at self.ip={}",
-					self.mem[self.ip], self.ip)
+				panic!("Unrecognized opcode {} at self.ip={} (should have been caught earlier", self.mem[self.ip as usize], self.ip)
 			}
+		}
+
+		match rvalue {
+			None => Ok(()),
+			Some(v) => self.checked_write(params.dest.unwrap(), v)
 		}
 	}
 
-	fn evaluate_params(&self, instruction: &Instruction) -> ParamValues {
-		// TODO: detect & propagate range errors in dest
+	fn evaluate_params(&self, instruction: &Instruction)
+			-> Result<ParamValues, Error> {
 		match instruction.opcode {
-			1 | 2 | 7 | 8 => ParamValues {
-				arg0: Some(self.lvalue(&instruction, 0)),
-				arg1: Some(self.lvalue(&instruction, 1)),
-				dest: Some(self.mem[self.ip + 3] as usize),
-			},
-			3 => ParamValues {
+			1 | 2 | 7 | 8 => Ok(ParamValues {
+				arg0: Some(self.lvalue(&instruction, 0)?),
+				arg1: Some(self.lvalue(&instruction, 1)?),
+				dest: Some(self.checked_read(self.ip + 3)?),
+			}),
+			3 => Ok(ParamValues {
 				arg0: None,
 				arg1: None,
-				dest: Some(self.mem[self.ip + 1] as usize),
-			},
-			4 => ParamValues {
-				arg0: Some(self.lvalue(&instruction, 0)),
+				dest: Some(self.checked_read(self.ip + 1)?),
+			}),
+			4 => Ok(ParamValues {
+				arg0: Some(self.lvalue(&instruction, 0)?),
 				arg1: None,
 				dest: None
-			},
-			5 | 6 => ParamValues {
-				arg0: Some(self.lvalue(&instruction, 0)),
-				arg1: Some(self.lvalue(&instruction, 1)),
+			}),
+			5 | 6 => Ok(ParamValues {
+				arg0: Some(self.lvalue(&instruction, 0)?),
+				arg1: Some(self.lvalue(&instruction, 1)?),
 				dest: None
-			},
-			_ => {
-				panic!("Unrecognized opcode {} at self.ip={}", self.mem[self.ip], self.ip);
+			}),
+			_ => Err(Error::InvalidOpcode {opcode: self.mem[self.ip as usize], ip: self.ip as i32})
+		}
+	}
+
+	fn lvalue(&self, instruction: &Instruction, param_ix: usize)
+			-> Result<i32, Error> {
+		let x = self.checked_read(self.ip + param_ix as i32 + 1)?;
+		match instruction.param_mode(param_ix) {
+			Mode::Immed => Ok(x),
+			Mode::Pos => self.checked_read(x)
+		}
+	}
+
+	fn checked_read(&self, index: i32) -> Result<i32, Error> {
+		match usize::try_from(index) {
+			Err(_) => Err(Error::OutOfBoundsRead {index}),
+			Ok(i) => match self.mem.get(i) {
+				None => Err(Error::OutOfBoundsRead {index}),
+				Some(v) => Ok(*v)
 			}
 		}
 	}
 
-	fn lvalue(&self, instruction: &Instruction, param_ix: usize) -> i32 {
-		// TODO: detect & propagate bounds errors
-		let x = self.mem[self.ip + param_ix + 1];
-		match instruction.param_mode(param_ix) {
-			Mode::Immed => x,
-			Mode::Pos => self.mem[x as usize]
+	fn checked_write(&mut self, index: i32, value: i32) -> Result<(), Error> {
+		match usize::try_from(index) {
+			Err(_) => Err(Error::OutOfBoundsWrite {index}),
+			Ok(i) => match self.mem.get_mut(i) {
+				None => Err(Error::OutOfBoundsWrite {index}),
+				Some(elem) => {
+					*elem = value;
+					Ok(())
+				}
+			}
 		}
 	}
 }
@@ -223,14 +264,14 @@ impl Machine {
 #[test]
 fn test_execute() {
 	let mut machine = Machine::new(vec![1,9,10,3,2,3,11,0,99,30,40,50]);
-	machine.execute();
+	machine.execute().unwrap();
 	assert_eq!(machine.mem, vec![3500,9,10,70,2,3,11,0,99,30,40,50]);
 }
 
 #[test]
 fn test_execute_negative() {
 	let mut machine = Machine::new(vec![1,5,6,7,99,-5,3,0]);
-	machine.execute();
+	machine.execute().unwrap();
 	assert_eq!(machine.mem, vec![1,5,6,7,99,-5,3,-2]);
 }
 
@@ -238,61 +279,61 @@ fn test_execute_negative() {
 fn test_execute_input() {
 	let mut machine = Machine::new(vec![3,4,99,0,0]);
 	machine.input = Some(5);
-	machine.execute();
+	machine.execute().unwrap();
 	assert_eq!(machine.mem, vec![3,4,99,0,5]);
 }
 
 #[test]
 fn test_execute_output() {
 	let mut machine = Machine::new(vec![4,3,99,12345]);
-	machine.execute();
+	machine.execute().unwrap();
 	assert_eq!(machine.output, vec![12345]);
 }
 
 #[test]
 fn test_execute_jump_if_true() {
 	let mut machine1 = Machine::new(vec![1105,0,4,99,104,1,99]);
-	machine1.execute();
+	machine1.execute().unwrap();
 	assert_eq!(machine1.output, vec![]);
 	let mut machine2 = Machine::new(vec![1105,1,4,99,4,1,99]);
-	machine2.execute();
+	machine2.execute().unwrap();
 	assert_eq!(machine2.output, vec![1]);
 }
 
 #[test]
 fn test_execute_jump_if_false() {
 	let mut machine1 = Machine::new(vec![1106,1,4,99,4,1,99]);
-	machine1.execute();
+	machine1.execute().unwrap();
 	assert_eq!(machine1.output, vec![]);
 
 	let mut machine2 = Machine::new(vec![1106,0,4,99,104,1,99]);
-	machine2.execute();
+	machine2.execute().unwrap();
 	assert_eq!(machine2.output, vec![1]);
 }
 
 #[test]
 fn test_less_than() {
 	let mut machine1 = Machine::new(vec![1107,1,2,5,99,-1]);
-	machine1.execute();
+	machine1.execute().unwrap();
 	assert_eq!(machine1.mem, vec![1107,1,2,5,99,1]);
 
 	let mut machine2 = Machine::new(vec![1107,2,2,5,99,-1]);
-	machine2.execute();
+	machine2.execute().unwrap();
 	assert_eq!(machine2.mem, vec![1107,2,2,5,99,0]);
 }
 
 #[test]
 fn test_equal() {
 	let mut machine1 = Machine::new(vec![1108,1,2,5,99,-1]);
-	machine1.execute();
+	machine1.execute().unwrap();
 	assert_eq!(machine1.mem, vec![1108,1,2,5,99,0]);
 
 	let mut machine2 = Machine::new(vec![1108,2,2,5,99,-1]);
-	machine2.execute();
+	machine2.execute().unwrap();
 	assert_eq!(machine2.mem, vec![1108,2,2,5,99,1]);
 
 	let mut machine3 = Machine::new(vec![1108,1,0,5,99,-1]);
-	machine3.execute();
+	machine3.execute().unwrap();
 	assert_eq!(machine3.mem, vec![1108,1,0,5,99,0]);
 }
 
@@ -300,17 +341,47 @@ fn test_equal() {
 fn test_execute_immediate_mode() {
 	let mut machine = Machine::new(vec![1102,4,3,5,99,0]);
 	machine.input = Some(5);
-	machine.execute();
+	machine.execute().unwrap();
 	assert_eq!(machine.mem, vec![1102,4,3,5,99,12]);
 }
 
-fn debug(machine: &mut Machine) {
+#[test]
+fn test_execute_detects_oob_reads() {
+	let mut machine = Machine::new(vec![2, 500, 0, 0, 99]);
+	assert_eq!(machine.execute(), Err(Error::OutOfBoundsRead {index: 500}));
+	let mut machine2 = Machine::new(vec![2, 0, 500, 0, 99]);
+	assert_eq!(machine2.execute(), Err(Error::OutOfBoundsRead {index: 500}));
+	let mut machine3 = Machine::new(vec![2, 0, -1, 0, 99]);
+	assert_eq!(machine3.execute(), Err(Error::OutOfBoundsRead {index: -1}));
+}
+
+#[test]
+fn test_execute_detects_oob_writes() {
+	let mut machine = Machine::new(vec![1102, 0, 0, 500, 99]);
+	assert_eq!(machine.execute(), Err(Error::OutOfBoundsWrite {index: 500}));
+	let mut machine2 = Machine::new(vec![1102, 0, 0, -1, 99]);
+	assert_eq!(machine2.execute(), Err(Error::OutOfBoundsWrite {index: -1}));
+}
+
+#[test]
+fn test_execute_detects_invalid_opcode() {
+	let mut machine = Machine::new(vec![55]);
+	assert_eq!(machine.execute(), Err(Error::InvalidOpcode {opcode: 55, ip: 0}));
+}
+
+#[test]
+fn test_execute_detects_oob_jumps() {
+	let mut machine = Machine::new(vec![1105,1,-1,99]);
+	assert_eq!(machine.execute(), Err(Error::IpOutOfRange {ip: -1}));
+}
+
+fn debug(mut machine: &mut Machine) {
 	println!("{} words of memory", machine.mem.len());
 	show_machine_state(&machine);
 
 	loop {
 		print!("debugger> ");
-		io::stdout().flush();
+		io::stdout().flush().unwrap();
 		let line: String = read!("{}\n");
 		let tokens: Vec<&str> = line.split(" ").collect();
 
@@ -320,17 +391,13 @@ fn debug(machine: &mut Machine) {
 				dump_mem(tokens, &machine);
 			},
 			"s" | "step" => {
-				machine.do_current_instruction();
-				show_machine_state(&machine);
+				debug_step(&mut machine);
 			},
 			"r" | "run" => {
-				while machine.mem[machine.ip] != 99 {
-					machine.do_current_instruction();
-					if machine.mem[machine.ip] != 99 {
-						show_machine_state(&machine);
-					}
+				while machine.mem[machine.ip as usize] != 99 &&
+						debug_step(&mut machine) {
 				}
-			}
+			},
 			_ => {
 				println!("Commands:");
 				println!("mem [start end]: dump memory");
@@ -342,9 +409,26 @@ fn debug(machine: &mut Machine) {
 	}
 }
 
+fn debug_step(machine: &mut Machine) -> bool {
+	let ok = match machine.do_current_instruction() {
+		Ok(_) => true,
+		Err(e) => {
+			println!("{:#?}", e);
+			false
+		}
+	};
+
+	if machine.mem[machine.ip as usize] != 99 {
+		show_machine_state(&machine);
+	}
+
+	ok
+}
+
 fn show_machine_state(machine: &Machine) {
 	println!("ip={}", machine.ip);
-	println!("mem[ip..ip+3]={:?}", &machine.mem[machine.ip..min(machine.ip+4, machine.mem.len() - 1)]);
+	let ip = machine.ip as usize;
+	println!("mem[ip..ip+3]={:?}", &machine.mem[ip..min(ip+4, machine.mem.len() - 1)]);
 	println!("instruction at ip: {:?}", machine.current_instruction());
 }
 
