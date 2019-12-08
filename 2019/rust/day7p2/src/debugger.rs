8f5ed3ceed48aaee05470980bbbd2e37eab1e845
Steve Gravrock <sdg@panix.com>
2019-12-08 10:21:58 -0800

    Day 7 part 2 (rust): Moved machine, debugger, and puzzle input to their own files

---
+++ 2019/rust/day7p2/src/debugger.rs
@@ -0,0 +1,85 @@
+use crate::machine::Machine;
+use std::io;
+use std::io::Write;
+use std::cmp::min;
+
+pub fn debug(mut machine: &mut Machine) {
+	println!("{} words of memory", machine.mem.len());
+	show_machine_state(&machine);
+
+	loop {
+		print!("debugger> ");
+		io::stdout().flush().unwrap();
+		let line: String = read!("{}\n");
+		let tokens: Vec<&str> = line.split(" ").collect();
+
+		match tokens[0] {
+			"q" | "quit" => return,
+			"mem" => {
+				dump_mem(tokens, &machine);
+			},
+			"s" | "step" => {
+				debug_step(&mut machine);
+			},
+			"r" | "run" => {
+				while machine.mem[machine.ip as usize] != 99 &&
+						debug_step(&mut machine) {
+				}
+			},
+			_ => {
+				println!("Commands:");
+				println!("mem [start end]: dump memory");
+				println!("s, step:         perform the instruction at ip");
+				println!("r, run:          run from current position");
+				println!("q, quit:         exit");
+			}
+		}
+	}
+}
+
+fn debug_step(machine: &mut Machine) -> bool {
+	let ok = match machine.do_current_instruction() {
+		Ok(_) => true,
+		Err(e) => {
+			println!("{:#?}", e);
+			false
+		}
+	};
+
+	if machine.mem[machine.ip as usize] != 99 {
+		show_machine_state(&machine);
+	}
+
+	ok
+}
+
+fn show_machine_state(machine: &Machine) {
+	println!("ip={}", machine.ip);
+	let ip = machine.ip as usize;
+	println!("mem[ip..ip+3]={:?}", &machine.mem[ip..min(ip+4, machine.mem.len() - 1)]);
+	println!("instruction at ip: {:?}", machine.current_instruction());
+}
+
+fn dump_mem(cmd: Vec<&str>, machine: &Machine) {
+	match cmd.len() {
+		1 => println!("{:?}", machine.mem),
+		3 => {
+			match cmd[1].parse::<usize>() {
+				Ok(start) => {
+					match cmd[2].parse::<usize>() {
+						Ok(end) => {
+							if start < end {
+								println!("{:?}", &machine.mem[start..end+1]);
+							} else {
+								println!("Start must be before end");
+							}
+						},
+						Err(_) => println!("End must be a nonnegative int")
+					}
+				},
+				Err(_) => println!("Start must be a nonnegative int")
+			}
+		},
+		_ => println!("Usage: mem [start end]")
+	};
+}
