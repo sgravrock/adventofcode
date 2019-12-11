3426c1b183fc3e59fc9a986d605fcdaa9aa8ade9
Steve Gravrock <sdg@panix.com>
2019-12-10 18:55:58 -0800

    Day 10 part 1 (rust): put tests last

--- 2019/rust/day10p1/src/main.rs
+++ 2019/rust/day10p1/src/main.rs
@@ -36,6 +36,85 @@ fn best_location(grid: &Grid) -> Location {
 		.unwrap()
 }
 
+fn reachable_asteroids(grid: &Grid, observer: Coord) -> Vec<Coord> {
+	all_coords(&grid)
+		.into_iter()
+		.filter(|dest| {
+			*dest != observer &&
+				grid.cells[dest.1 * grid.width + dest.0] &&
+				is_reachable(&grid, observer, *dest)
+		})
+		.collect()
+}
+
+fn all_coords(grid: &Grid) -> Vec<Coord> {
+	(0..grid.height)
+		.flat_map(|y| {
+			(0..grid.width).map(move |x| (x, y))
+		})
+		.collect()
+}
+
+fn is_reachable(grid: &Grid, mut observer: Coord, mut dest: Coord) -> bool {
+	assert!(observer.0 < grid.width);
+	assert!(observer.1 < grid.cells.len() / grid.width);
+	assert!(dest.0 < grid.width);
+	assert!(dest.1 < grid.cells.len() / grid.width);
+
+	// TODO: Unify this with the general case below somehow
+	if observer.0 == dest.0 {
+		if observer.1 > dest.1 {
+			swap(&mut observer, &mut dest);
+		}
+
+		for y2 in (observer.1 + 1)..dest.1 {
+			if grid.cells[y2 * grid.width + observer.0] {
+				return false;
+			}
+		}
+	} else {
+		if observer.0 > dest.0 {
+			swap(&mut observer, &mut dest);
+		}
+	
+		let rise = dest.1 as f32 - observer.1 as f32;
+		let slope = rise / (dest.0 - observer.0) as f32;
+	
+		for x in (observer.0 + 1)..dest.0 {
+			let dx = x - observer.0;
+			let dy = (dx as f32) * slope;
+	
+			if (dy - dy.round()).abs() < 0.01 {
+				let y = (observer.1 as isize + dy.round() as isize) as usize;
+				if grid.cells[y * grid.width + x] {
+					return false;
+				}
+			}
+		}
+	}
+
+	true
+}
+
+fn str_to_grid(input: &str) -> Grid {
+	let whitespace = Regex::new(r"[\s]+").unwrap();
+	let width = match whitespace.replace(input, "").find('\n') {
+		Some(n) => n,
+		None => input.len()
+	};
+	let cells: Vec<bool> = whitespace.replace_all(input, "")
+		.chars()
+		.map(|c| match c {
+			'.' => false,
+			'#' => true,
+			_ => panic!("Unexpected input character: {}", c)
+		})
+		.collect();
+	let height = cells.len() / width;
+	Grid { cells, width, height }
+}
+
+
 #[test]
 fn test_best_location() {
 	let grid1 = str_to_grid("
@@ -114,66 +193,6 @@ fn test_best_location() {
 	assert_eq!(best_location(&grid5), Location {coord: (11,13), n_reachable: 210});
 }
 
-fn reachable_asteroids(grid: &Grid, observer: Coord) -> Vec<Coord> {
-	all_coords(&grid)
-		.into_iter()
-		.filter(|dest| {
-			*dest != observer &&
-				grid.cells[dest.1 * grid.width + dest.0] &&
-				is_reachable(&grid, observer, *dest)
-		})
-		.collect()
-}
-
-fn all_coords(grid: &Grid) -> Vec<Coord> {
-	(0..grid.height)
-		.flat_map(|y| {
-			(0..grid.width).map(move |x| (x, y))
-		})
-		.collect()
-}
-
-fn is_reachable(grid: &Grid, mut observer: Coord, mut dest: Coord) -> bool {
-	assert!(observer.0 < grid.width);
-	assert!(observer.1 < grid.cells.len() / grid.width);
-	assert!(dest.0 < grid.width);
-	assert!(dest.1 < grid.cells.len() / grid.width);
-
-	// TODO: Unify this with the general case below somehow
-	if observer.0 == dest.0 {
-		if observer.1 > dest.1 {
-			swap(&mut observer, &mut dest);
-		}
-
-		for y2 in (observer.1 + 1)..dest.1 {
-			if grid.cells[y2 * grid.width + observer.0] {
-				return false;
-			}
-		}
-	} else {
-		if observer.0 > dest.0 {
-			swap(&mut observer, &mut dest);
-		}
-	
-		let rise = dest.1 as f32 - observer.1 as f32;
-		let slope = rise / (dest.0 - observer.0) as f32;
-	
-		for x in (observer.0 + 1)..dest.0 {
-			let dx = x - observer.0;
-			let dy = (dx as f32) * slope;
-	
-			if (dy - dy.round()).abs() < 0.01 {
-				let y = (observer.1 as isize + dy.round() as isize) as usize;
-				if grid.cells[y * grid.width + x] {
-					return false;
-				}
-			}
-		}
-	}
-
-	true
-}
-
 #[test]
 fn test_reachable_asteroids() {
 	let grid = str_to_grid("
@@ -194,20 +213,6 @@ fn test_reachable_asteroids() {
 		vec_to_set(expected)
 	);
 }
-
-/*
-#[test]
-fn test_reachable_asteroids_extra_examples() {
-	let grid = str_to_grid("
-		.#..#
-		.....
-		#####
-		....#
-		...##
-	");
-	3,3
-}
-*/
 
 #[test]
 fn test_is_reachable_obviously_unblocked() {
@@ -263,24 +268,6 @@ fn test_is_reachable_not_blocked_at_an_angle() {
 		...#...
 	");
 	assert_eq!(is_reachable(&grid, (6, 2), (0, 0)), true);
-}
-
-fn str_to_grid(input: &str) -> Grid {
-	let whitespace = Regex::new(r"[\s]+").unwrap();
-	let width = match whitespace.replace(input, "").find('\n') {
-		Some(n) => n,
-		None => input.len()
-	};
-	let cells: Vec<bool> = whitespace.replace_all(input, "")
-		.chars()
-		.map(|c| match c {
-			'.' => false,
-			'#' => true,
-			_ => panic!("Unexpected input character: {}", c)
-		})
-		.collect();
-	let height = cells.len() / width;
-	Grid { cells, width, height }
 }
 
 #[test]
