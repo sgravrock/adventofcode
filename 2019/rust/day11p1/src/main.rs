ed3680cddfc16db5bc63d1ae880d95cd6321eac2
Steve Gravrock <sdg@panix.com>
2019-12-17 20:18:33 -0800

    Day 11 part 1 (rust): From day 13

---
+++ 2019/rust/day11p1/src/main.rs
@@ -0,0 +1,20 @@
+#[macro_use] extern crate text_io;
+mod input;
+mod machine;
+mod debugger;
+use machine::Machine;
+use debugger::debug;
+
+fn main() {
+}
+
+fn execute_or_debug(mut machine: &mut Machine) {
+	match machine.execute() {
+		Ok(_) => {},
+		Err(error) => {
+			println!("Execution failed: {:?}", error);
+			debug(&mut machine);
+			panic!("Aborting because of previous execution failure");
+		}
+	}
+}
