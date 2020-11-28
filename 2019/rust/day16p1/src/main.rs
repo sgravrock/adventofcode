acae8410dcdb842f519ae59e656c47268b43dc1e
Steve Gravrock <sdg@panix.com>
2020-11-28 14:44:08 -0800

    Day 16 part 1 (Rust): Much slower, but works.

--- 2019/rust/day16p1/src/main.rs
+++ 2019/rust/day16p1/src/main.rs
@@ -1,4 +1,9 @@
+mod input;
+use std::convert::TryInto;
+
 fn main() {
+	println!("{:?}", fft_first_8(str_to_vec(input::puzzle_input()), 100));
+	// 30550349
 }
 
 fn str_to_vec(input: &str) -> Vec<u32> {
@@ -7,85 +12,99 @@ fn str_to_vec(input: &str) -> Vec<u32> {
 		.collect()
 }
 
-fn fft(input: Vec<u32>, times: u32) -> Vec<u32> {
-	(0..times).fold(input, |prev_signal, _| fft_once(prev_signal))
-}
-
-fn fft_once(signal: Vec<u32>) -> Vec<u32> {
-	(0..signal.len())
-		.map(|i| {
-			signal.iter()
-				.zip(fft_multipliers(i))
-				.map(|(sv, mult)| *sv as i32 * mult)
-				.sum::<i32>()
-				.abs() as u32 % 10
-		})
-		.take(8)
-		.collect()
-}
-
-fn fft_multipliers(digit_ix: usize) -> impl Iterator<Item=i32> {
-	static BASE_PATTERN: [i32; 4] = [0, 1, 0, -1];
-	BASE_PATTERN
-		.iter()
-		.cloned()
-		.flat_map(move |p| std::iter::repeat(p).take(digit_ix + 1))
-		.cycle()
-		.skip(1)
+fn fft_first_8(input: Vec<u32>, times: u32) -> Vec<u32> {
+	let mut result = fft(input, times);
+	result.truncate(8);
+	result
 }
 
 #[test]
-fn test_fft_short() {
-	assert_eq!(fft(str_to_vec("12345678"), 1), str_to_vec("48226158"));
-	assert_eq!(fft(str_to_vec("12345678"), 2), str_to_vec("34040438"));
-	assert_eq!(fft(str_to_vec("12345678"), 3), str_to_vec("03415518"));
-	assert_eq!(fft(str_to_vec("12345678"), 4), str_to_vec("01029498"));
-}
-
-#[test]
-fn test_fft_long_1() {
+fn test_fft_first_8() {
 	assert_eq!(
-		fft(str_to_vec("80871224585914546619083218645595"), 100),
+		fft_first_8(str_to_vec("80871224585914546619083218645595"), 100),
 		str_to_vec("24176176")
 	);
-}
-
-#[test]
-fn test_fft_long_2() {
 	assert_eq!(
-		fft(str_to_vec("19617804207202209144916044189917"), 100),
+		fft_first_8(str_to_vec("19617804207202209144916044189917"), 100),
 		str_to_vec("73745418")
 	);
-}
-
-#[test]
-fn test_fft_long_3() {
 	assert_eq!(
-		fft(str_to_vec("69317163492948606335995924319873"), 100),
+		fft_first_8(str_to_vec("69317163492948606335995924319873"), 100),
 		str_to_vec("52432133")
 	);
 }
 
-#[test]
-fn test_fft_multipliers_0() {
-	let actual: Vec<i32> = fft_multipliers(0).take(8).collect();
-	assert_eq!(actual, vec![1, 0, -1, 0, 1, 0, -1, 0]);
+fn fft(input: Vec<u32>, times: u32) -> Vec<u32> {
+	if times == 0 {
+		return input;
+	}
+
+	let mut next_state = Vec::with_capacity(input.len());
+
+	for i in 0..input.len() {
+		next_state.push(fft_digit(&input, i));
+	}
+
+	fft(next_state, times - 1)
+}
+
+fn fft_digit(input: &Vec<u32>, output_pos: usize) -> u32 {
+	let pat = repeated_pattern(output_pos, input.len());
+	let mut total: i32 = 0;
+
+	for i in 0..input.len() {
+		let v: i32 = input[i].try_into().unwrap();
+		total += v * pat[i];
+	}
+
+	(total % 10).abs().try_into().unwrap()
 }
 
 #[test]
-fn test_fft_multipliers_1() {
-	let actual: Vec<i32> = fft_multipliers(1).take(8).collect();
-	assert_eq!(actual, vec![0, 1, 1, 0, 0, -1, -1, 0]);
+fn test_fft() {
+	let input = str_to_vec("12345678");
+	assert_eq!(fft(input.clone(), 1), str_to_vec("48226158"));
+	assert_eq!(fft(input.clone(), 2), str_to_vec("34040438"));
+	assert_eq!(fft(input.clone(), 3), str_to_vec("03415518"));
+	assert_eq!(fft(input.clone(), 4), str_to_vec("01029498"));
+}
+
+fn pattern(output_pos: usize) -> Vec<i32> {
+	let base = &[0, 1, 0, -1];
+	let mut result = Vec::with_capacity(
+		(4 * output_pos).try_into().unwrap()
+	);
+
+	for x in base {
+		for _ in 0..(output_pos + 1) {
+			result.push(*x);
+		}
+	}
+
+	result
 }
 
 #[test]
-fn test_fft_multipliers_repeats() {
-	let actual: Vec<i32> = fft_multipliers(1).take(16).collect();
+fn test_pattern() {
+	assert_eq!(pattern(0), vec![0, 1, 0, -1]);
+	assert_eq!(pattern(2), vec![0, 0, 0, 1, 1, 1, 0, 0, 0, -1, -1, -1]);
+}
+
+fn repeated_pattern(output_pos: usize, len: usize) -> Vec<i32> {
+	let to_repeat = pattern(output_pos);
+	let mut result = Vec::with_capacity(len);
+
+	for i in 0..len {
+		result.push(to_repeat[(i + 1) % to_repeat.len()]);
+	}
+
+	result
+}
+
+#[test]
+fn test_repeated_pattern() {
 	assert_eq!(
-		actual,
-		vec![
-			0, 1, 1, 0, 0, -1, -1, 0,
-			0, 1, 1, 0, 0, -1, -1, 0,
-		]
+		repeated_pattern(1, 18),
+		vec![0, 1, 1, 0, 0, -1, -1, 0, 0, 1, 1, 0, 0, -1, -1, 0, 0, 1]
 	);
 }
