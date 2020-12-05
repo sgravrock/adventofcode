ef870b14af335c034931e9a9fc77dae246d56bce
Steve Gravrock <sdg@panix.com>
2020-12-05 12:04:07 -0800

    Day 4 part 2 (Rust): refactoring

--- 2020/rust/day4p2/src/main.rs
+++ 2020/rust/day4p2/src/main.rs
@@ -24,6 +24,19 @@ impl Field {
 	}
 }
 
+struct Validator {
+	field_name: &'static str,
+	predicate: fn(&str) -> bool,
+}
+
+impl Validator {
+	fn is_valid(&self, fields: &Vec<Field>) -> bool {
+		fields.iter().find(|f| f.name == self.field_name)
+			.and_then(|f| Some((self.predicate)(&f.value)))
+			.unwrap_or(false)
+	}
+}
+
 fn num_valid(input: &str) -> u32 {
 	let mut cur_fields: Vec<Field> = Vec::new();
 	let mut result = 0;
@@ -86,38 +99,36 @@ iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719";
 }
 
 fn is_valid(fields: &Vec<Field>) -> bool {
-	VALIDATORS.iter().all(|f| f(&fields))
+	VALIDATORS.iter().all(|v| v.is_valid(&fields))
 }
 
-fn byr_valid(fields: &Vec<Field>) -> bool {
-	is_num_in_range(&fields, "byr", 1920..=2002)
+fn byr_valid(value: &str) -> bool {
+	is_num_in_range(value, 1920..=2002)
 }
 
 #[test]
 fn test_byr_valid() {
-	assert_eq!(byr_valid(&vec![]), false);
-	assert_eq!(byr_valid(&vec![Field::new("byr", "not a number")]), false);
-	assert_eq!(byr_valid(&vec![Field::new("byr", "1920")]), true);
-	assert_eq!(byr_valid(&vec![Field::new("byr", "2002")]), true);
-	assert_eq!(byr_valid(&vec![Field::new("byr", "2003")]), false);
-	assert_eq!(byr_valid(&vec![Field::new("byr", "1919")]), false);
+	assert_eq!(byr_valid("not a number"), false);
+	assert_eq!(byr_valid("1920"), true);
+	assert_eq!(byr_valid("2002"), true);
+	assert_eq!(byr_valid("2003"), false);
+	assert_eq!(byr_valid("1919"), false);
 }
 
-fn iyr_valid(fields: &Vec<Field>) -> bool {
-	is_num_in_range(&fields, "iyr", 2010..=2020)
+fn iyr_valid(value: &str) -> bool {
+	is_num_in_range(value, 2010..=2020)
 }
 
-fn eyr_valid(fields: &Vec<Field>) -> bool {
-	is_num_in_range(&fields, "eyr", 2010..=2030)
+fn eyr_valid(value: &str) -> bool {
+	is_num_in_range(value, 2010..=2030)
 }
 
-fn hgt_valid(fields: &Vec<Field>) -> bool {
+fn hgt_valid(value: &str) -> bool {
 	lazy_static! {
 		static ref RE: Regex = Regex::new(r"^([\d]+)(in|cm)$").unwrap();
 	}
 
-	fields.iter().find(|f| f.name == "hgt")
-		.and_then(|f| RE.captures(&f.value))
+	RE.captures(&value)
 		.and_then(|caps| {
 			caps[1].parse::<u32>().ok().and_then(|v| {
 				Some((v, caps[2].to_string()))
@@ -136,103 +147,89 @@ fn hgt_valid(fields: &Vec<Field>) -> bool {
 
 #[test]
 fn test_hgt_valid() {
-	assert_eq!(hgt_valid(&vec![]), false);
-	assert_eq!(hgt_valid(&vec![Field::new("hgt", "tall")]), false);
-	assert_eq!(hgt_valid(&vec![Field::new("hgt", "60furlongs")]), false);
-	assert_eq!(hgt_valid(&vec![Field::new("hgt", "59in")]), true);
-	assert_eq!(hgt_valid(&vec![Field::new("hgt", "76in")]), true);
-	assert_eq!(hgt_valid(&vec![Field::new("hgt", "58in")]), false);
-	assert_eq!(hgt_valid(&vec![Field::new("hgt", "77in")]), false);
-	assert_eq!(hgt_valid(&vec![Field::new("hgt", "150cm")]), true);
-	assert_eq!(hgt_valid(&vec![Field::new("hgt", "193cm")]), true);
-	assert_eq!(hgt_valid(&vec![Field::new("hgt", "149cm")]), false);
-	assert_eq!(hgt_valid(&vec![Field::new("hgt", "194cm")]), false);
+	assert_eq!(hgt_valid("tall"), false);
+	assert_eq!(hgt_valid("60furlongs"), false);
+	assert_eq!(hgt_valid("59in"), true);
+	assert_eq!(hgt_valid("76in"), true);
+	assert_eq!(hgt_valid("58in"), false);
+	assert_eq!(hgt_valid("77in"), false);
+	assert_eq!(hgt_valid("150cm"), true);
+	assert_eq!(hgt_valid("193cm"), true);
+	assert_eq!(hgt_valid("149cm"), false);
+	assert_eq!(hgt_valid("194cm"), false);
 }
 
-fn hcl_valid(fields: &Vec<Field>) -> bool {
+fn hcl_valid(value: &str) -> bool {
 	lazy_static! {
 		static ref RE: Regex = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
 	}
 
-	fields.iter().find(|f| f.name == "hcl")
-		.and_then(|f| Some(RE.is_match(&f.value)))
-		.unwrap_or(false)
+	RE.is_match(&value)
 }
 
 #[test]
 fn test_hcl_valid() {
-	assert_eq!(hcl_valid(&vec![]), false);
-	assert_eq!(hcl_valid(&vec![Field::new("hcl", "aaaaaa")]), false);
-	assert_eq!(hcl_valid(&vec![Field::new("hcl", "#abcdef")]), true);
-	assert_eq!(hcl_valid(&vec![Field::new("hcl", "#012345")]), true);
-	assert_eq!(hcl_valid(&vec![Field::new("hcl", "#6789ab")]), true);
-	assert_eq!(hcl_valid(&vec![Field::new("hcl", "#abcdeff")]), false);
-	assert_eq!(hcl_valid(&vec![Field::new("hcl", "#abcde")]), false);
-	assert_eq!(hcl_valid(&vec![Field::new("hcl", "#ghijkl")]), false);
+	assert_eq!(hcl_valid("aaaaaa"), false);
+	assert_eq!(hcl_valid("#abcdef"), true);
+	assert_eq!(hcl_valid("#012345"), true);
+	assert_eq!(hcl_valid("#6789ab"), true);
+	assert_eq!(hcl_valid("#abcdeff"), false);
+	assert_eq!(hcl_valid("#abcde"), false);
+	assert_eq!(hcl_valid("#ghijkl"), false);
 }
 
-fn ecl_valid(fields: &Vec<Field>) -> bool {
-	fields.iter().find(|f| f.name == "ecl")
-		.and_then(|f| {
-			Some(f.value == "amb" || f.value == "blu" || f.value == "brn" ||
-				f.value == "gry" || f.value == "grn" || f.value == "hzl" ||
-				f.value == "oth")
-		})
-		.unwrap_or(false)
+fn ecl_valid(value: &str) -> bool {
+	value == "amb" || value == "blu" || value == "brn" ||
+		value == "gry" || value == "grn" || value == "hzl" ||
+		value == "oth"
 }
 
 #[test]
 fn test_ecl_valid() {
-	assert_eq!(ecl_valid(&vec![]), false);
-	assert_eq!(ecl_valid(&vec![Field::new("ecl", "amb")]), true);
-	assert_eq!(ecl_valid(&vec![Field::new("ecl", "blu")]), true);
-	assert_eq!(ecl_valid(&vec![Field::new("ecl", "brn")]), true);
-	assert_eq!(ecl_valid(&vec![Field::new("ecl", "gry")]), true);
-	assert_eq!(ecl_valid(&vec![Field::new("ecl", "grn")]), true);
-	assert_eq!(ecl_valid(&vec![Field::new("ecl", "hzl")]), true);
-	assert_eq!(ecl_valid(&vec![Field::new("ecl", "oth")]), true);
-	assert_eq!(ecl_valid(&vec![Field::new("ecl", "anything else")]), false);
+	assert_eq!(ecl_valid("amb"), true);
+	assert_eq!(ecl_valid("blu"), true);
+	assert_eq!(ecl_valid("brn"), true);
+	assert_eq!(ecl_valid("gry"), true);
+	assert_eq!(ecl_valid("grn"), true);
+	assert_eq!(ecl_valid("hzl"), true);
+	assert_eq!(ecl_valid("oth"), true);
+	assert_eq!(ecl_valid("anything else"), false);
 }
 
-fn pid_valid(fields: &Vec<Field>) -> bool {
+fn pid_valid(value: &str) -> bool {
 	lazy_static! {
 		static ref RE: Regex = Regex::new(r"^[\d]{9}$").unwrap();
 	}
 
-	fields.iter().find(|f| f.name == "pid")
-		.and_then(|f| Some(RE.is_match(&f.value)))
-		.unwrap_or(false)
+	RE.is_match(&value)
 }
 
 #[test]
 fn test_pid_valid() {
-	assert_eq!(pid_valid(&vec![]), false);
-	assert_eq!(pid_valid(&vec![Field::new("pid", "012345678")]), true);
-	assert_eq!(pid_valid(&vec![Field::new("pid", "123456789")]), true);
-	assert_eq!(pid_valid(&vec![Field::new("pid", "12345678")]), false);
-	assert_eq!(pid_valid(&vec![Field::new("pid", "0123456789")]), false);
-	assert_eq!(pid_valid(&vec![Field::new("pid", "a12345678")]), false);
+	assert_eq!(pid_valid("012345678"), true);
+	assert_eq!(pid_valid("123456789"), true);
+	assert_eq!(pid_valid("12345678"), false);
+	assert_eq!(pid_valid("0123456789"), false);
+	assert_eq!(pid_valid("a12345678"), false);
 }
 
 
 fn is_num_in_range(
-	fields: &Vec<Field>,
-	field_name: &str,
+	value: &str,
 	range: RangeInclusive<u32>
 ) -> bool {
-	fields.iter().find(|f| f.name == field_name)
-		.and_then(|target_field| target_field.value.parse::<u32>().ok())
-		.and_then(|val| Some(range.contains(&val)))
-		.unwrap_or(false)
+	match value.parse::<u32>() {
+		Ok(n) => range.contains(&n),
+		Err(_) => false
+	}
 }
 
-// TODO: can this type be simplfiied?
-static VALIDATORS: &[for<'r> fn(&'r Vec<Field>) -> bool; 7] = &[
-	byr_valid,
-	iyr_valid,
-	eyr_valid,
-	hgt_valid,
-	hcl_valid,
-	ecl_valid,
-	pid_valid,
+static VALIDATORS: &[Validator; 7] = &[
+	Validator { field_name: "byr", predicate: byr_valid },
+	Validator { field_name: "iyr", predicate: iyr_valid },
+	Validator { field_name: "eyr", predicate: eyr_valid },
+	Validator { field_name: "hgt", predicate: hgt_valid },
+	Validator { field_name: "hcl", predicate: hcl_valid },
+	Validator { field_name: "ecl", predicate: ecl_valid },
+	Validator { field_name: "pid", predicate: pid_valid },
 ];
