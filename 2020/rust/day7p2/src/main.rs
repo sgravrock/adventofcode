c628a48d81a4fc31a9475bc1401e830eb5b08f44
Steve Gravrock <sdg@panix.com>
2020-12-08 11:56:24 -0800

    Day 7 part 2 (Rust)

--- 2020/rust/day7p2/src/main.rs
+++ 2020/rust/day7p2/src/main.rs
@@ -1,41 +1,37 @@
 #[macro_use] extern crate lazy_static;
 extern crate regex;
 use regex::Regex;
-use std::collections::HashSet;
 use std::collections::HashMap;
 mod input;
 
 fn main() {
-	 let result = can_contain_shiny_gold(parse_rules(input::puzzle_input()));
-    println!("{}", result.len());
+	println!("{}", num_contained_in_shiny_gold(parse_rules(input::puzzle_input())));
+	// 1250
 }
 
-// Returns a map from bag color to what bags contain taht color,
-// the opposite of the data structured described by the input.
-fn parse_rules(input: &str) -> HashMap<String, Vec<String>> {
-	let mut result = HashMap::new();
-
-	for line in input.lines() {
-		lazy_static! {
-			static ref TOP_RE: Regex = Regex::new(
-				r"(.+) bags contain (.+)\.$"
-			).unwrap();
-			static ref CONTENTS_RE: Regex = Regex::new(
-				r"(?:\d+) ([^,]+) bag"
-			).unwrap();
-		}
-
-		let top_caps = TOP_RE.captures(&line).unwrap();
-		let color = top_caps[1].to_string();
-
-		for cap in CONTENTS_RE.captures_iter(&top_caps[2]) {
-			let contents_color = cap[1].to_string();
-			let v = result.entry(contents_color).or_insert(Vec::new());
-			v.push(color.clone());
-		}
-	}
-
-	result
+fn parse_rules(input: &str) -> HashMap<String, Vec<(u32, String)>> {
+	input.lines()
+		.map(|line| {
+			lazy_static! {
+				static ref TOP_RE: Regex = Regex::new(
+					r"(.+) bags contain (.+)\.$"
+				).unwrap();
+				static ref CONTENTS_RE: Regex = Regex::new(
+					r"(\d+) ([^,]+) bag"
+				).unwrap();
+			}
+	
+			let top_caps = TOP_RE.captures(&line).unwrap();
+			let color = top_caps[1].to_string();
+			let contents: Vec<(u32, String)> = CONTENTS_RE.captures_iter(&top_caps[2])
+				.map(|cap| {
+					let n: u32 = cap[1].parse().unwrap();
+					let contents_color = cap[2].to_string();
+					(n, contents_color)
+				}).collect();
+				(color, contents)
+		})
+		.collect()
 }
 
 #[test]
@@ -45,47 +41,41 @@ light blue bags contain 1 bright white bag.
 bright white bags contain 1 shiny gold bag.");
 	let mut expected = HashMap::new();
 	expected.insert(
+		"light red".to_string(),
+		vec![
+			(1, "bright white".to_string()),
+			(2, "muted yellow".to_string())
+		]
+	);
+	expected.insert(
+		"light blue".to_string(),
+		vec![(1, "bright white".to_string())]
+	);
+	expected.insert(
 		"bright white".to_string(),
-		vec!["light red".to_string(), "light blue".to_string()]
-	);
-	expected.insert(
-		"muted yellow".to_string(),
-		vec!["light red".to_string()]
-	);
-	expected.insert(
-		"shiny gold".to_string(),
-		vec!["bright white".to_string()]
+		vec![(1, "shiny gold".to_string())]
 	);
 	assert_eq!(actual, expected);
 }
 
-fn can_contain_shiny_gold(
-	rules: HashMap<String, Vec<String>>
-) -> HashSet<String> {
-
-	let mut result = HashSet::new();
-	can_contain(&mut result, &rules, "shiny gold".to_string());
-	result
+fn num_contained_in_shiny_gold(
+	rules: HashMap<String, Vec<(u32, String)>>
+) -> u32 {
+	num_contained_in(&rules, "shiny gold")
 }
 
-fn can_contain(
-	mut result: &mut HashSet<String>,
-	rules: &HashMap<String, Vec<String>>,
-	target: String
-) {
-	match rules.get(&target) {
-		None => { /* A "root" color, contained in nothing. */ },
-		Some(containers) => {
-			for container in containers {
-				result.insert(container.to_string());
-				can_contain(&mut result, &rules, container.to_string());
-			}
-		}
-	}
+fn num_contained_in(
+	rules: &HashMap<String, Vec<(u32, String)>>,
+	container: &str
+) -> u32 {
+	let n = rules[container].iter()
+		.map(|(qty, color)| qty * (1 + num_contained_in(&rules, color)))
+		.sum();
+	n
 }
 
 #[test]
-fn test_can_contain_shiny_gold() {
+fn test_num_contained_in_shiny_gold_small() {
 	let rules = parse_rules("light red bags contain 1 bright white bag, 2 muted yellow bags.
 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
 bright white bags contain 1 shiny gold bag.
@@ -95,11 +85,18 @@ dark olive bags contain 3 faded blue bags, 4 dotted black bags.
 vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags.");
-	let mut expected: HashSet<String> = HashSet::new();
-	expected.insert("bright white".to_string());
-	expected.insert("muted yellow".to_string());
-	expected.insert("dark orange".to_string());
-	expected.insert("light red".to_string());
+	
+	assert_eq!(num_contained_in_shiny_gold(rules), 32);
+}
 
-	assert_eq!(can_contain_shiny_gold(rules), expected);
+#[test]
+fn test_num_contained_in_shiny_gold_bigger() {
+	let rules = parse_rules("shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.");
+	assert_eq!(num_contained_in_shiny_gold(rules), 126);
 }
