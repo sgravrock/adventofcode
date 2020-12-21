e2e90275faa4b8bd63885198515fb7d15d724944
Steve Gravrock <sdg@panix.com>
2020-12-21 10:39:52 -0800

    Day 12 part 2 (Rust)

--- 2020/rust/day12p2/src/main.rs
+++ 2020/rust/day12p2/src/main.rs
@@ -1,25 +1,57 @@
 mod input;
+use std::ops::{AddAssign, Mul};
 
 fn main() {
     println!("{}", solve(input::puzzle_input()));
+	 // 46530
 }
 
-#[derive(PartialEq, Debug)]
-struct State {
+#[derive(PartialEq, Debug, Clone, Copy)]
+struct Coord {
 	x: i32,
 	y: i32,
-	heading: i32, // degrees
+}
+
+impl Coord {
+	fn rotate(self: &Coord, degrees: i32) -> Coord {
+		match normalize_angle(degrees) {
+			90 => Coord { x: self.y, y: -1 * self.x },
+			180 => Coord { x: -1 * self.x, y: -1 * self.y },
+			270 => Coord { x: -1 * self.y, y: self.x },
+			_ => panic!("Don't know how to rotate {} yet", degrees)
+		}
+	}
+}
+
+impl AddAssign<Coord> for Coord {
+	fn add_assign(&mut self, other: Self) {
+		self.x += other.x;
+		self.y += other.y;
+	}
+}
+
+impl Mul<i32> for Coord {
+	type Output = Self;
+
+	fn mul(self, rhs: i32) -> Self {
+		Coord { x: self.x * rhs, y: self.y * rhs }
+	}
+}
+
+#[derive(PartialEq, Debug, Clone, Copy)]
+struct State {
+	pos: Coord,
+	waypoint: Coord,
 }
 
 fn solve(input: &str) -> i32 {
 	let initial_state = State {
-		x: 0,
-		y: 0,
-		heading: 90
+		pos: Coord { x: 0, y: 0 },
+		waypoint: Coord { x: 10, y: 1 },
 	};
 
 	let end_state = input.lines().fold(initial_state, handle_instruction);
-	end_state.x.abs() + end_state.y.abs()
+	end_state.pos.x.abs() + end_state.pos.y.abs()
 }
 
 fn handle_instruction(mut state: State, instruction: &str) -> State {
@@ -28,31 +60,20 @@ fn handle_instruction(mut state: State, instruction: &str) -> State {
 	let val = chars.collect::<String>().parse::<i32>().unwrap();
 
 	match cmd {
-		'N' => state.y += val,
-		'S' => state.y -= val,
-		'E' => state.x += val,
-		'W' => state.x -= val,
-		'F' => {
-			match state.heading {
-				 0 => state.y += val,
-				 180 => state.y -= val,
-				 90 => state.x += val,
-				 270 => state.x -= val,
-				 _ => {
-				 	// The puzzle input produces other headings,
-					// but they all cancel each other out.
-				}
-			}
-		},
-		'L' => state.heading = normalize_heading(state.heading - val),
-		'R' => state.heading = normalize_heading(state.heading + val),
+		'N' => state.waypoint.y += val,
+		'S' => state.waypoint.y -= val,
+		'E' => state.waypoint.x += val,
+		'W' => state.waypoint.x -= val,
+		'F' => state.pos += state.waypoint * val,
+		'L' => state.waypoint = state.waypoint.rotate(-1 * val),
+		'R' => state.waypoint = state.waypoint.rotate(val),
 		_ => panic!("Don't know how to '{}'", cmd)
 	}
 
 	state
 }
 
-fn normalize_heading(degrees: i32) -> i32 {
+fn normalize_angle(degrees: i32) -> i32 {
 	let wrapped = degrees % 360;
 	if wrapped < 0 {
 		360 + wrapped
@@ -68,5 +89,61 @@ N3
 F7
 R90
 F11";
-	assert_eq!(solve(input), 25);
+	assert_eq!(solve(input), 286);
+}
+
+#[test]
+fn test_handle_instruction_f() {
+	let start = State {
+		pos: Coord { x: 0, y: 0 },
+		waypoint: Coord { x: 10, y: 1 },
+	};
+	assert_eq!(handle_instruction(start, "F10"), State {
+		pos: Coord { x: 100, y: 10 },
+		waypoint: start.waypoint,
+	});
+}
+
+#[test]
+fn test_handle_instruction_n() {
+	let start = State {
+		pos: Coord { x: 100, y: 10 },
+		waypoint: Coord { x: 10, y: 1 },
+	};
+	assert_eq!(handle_instruction(start, "N3"), State {
+		pos: start.pos,
+		waypoint: Coord { x: 10, y: 4 },
+	});
+}
+
+#[test]
+fn test_handle_instruction_r() {
+	let start = State {
+		pos: Coord { x: 170, y: 38 },
+		waypoint: Coord { x: 10, y: 4 },
+	};
+	assert_eq!(handle_instruction(start, "R90"), State {
+		pos: start.pos,
+		waypoint: Coord { x: 4, y: -10 },
+	});
+	assert_eq!(handle_instruction(start, "R180"), State {
+		pos: start.pos,
+		waypoint: Coord { x: -10, y: -4 },
+	});
+}
+
+#[test]
+fn test_handle_instruction_l() {
+	let start = State {
+		pos: Coord { x: 170, y: 38 },
+		waypoint: Coord { x: 10, y: 4 },
+	};
+	assert_eq!(handle_instruction(start, "L90"), State {
+		pos: start.pos,
+		waypoint: Coord { x: -4, y: 10 },
+	});
+	assert_eq!(handle_instruction(start, "L180"), State {
+		pos: start.pos,
+		waypoint: Coord { x: -10, y: -4 },
+	});
 }
