471c84d76df0ef618df2ccdcc2aaa2c4a692a44a
Steve Gravrock <sdg@panix.com>
2020-12-26 10:46:22 -0800

    Day 17 part 1 (Rust)

--- 2020/rust/day17p1/src/main.rs
+++ 2020/rust/day17p1/src/main.rs
@@ -1,25 +1,25 @@
+#[macro_use]
+extern crate lazy_static;
 mod input;
 mod grid;
+mod cube;
+use cube::{Cube, Coord};
 
 fn main() {
-	println!("{}", solve(grid::parse(input::puzzle_input())));
-	// 2346
+	let cube = Cube::parse_plane(input::puzzle_input(), Cell::Inactive);
+	println!("{}", solve(cube));
+	// 324
 }
 
 
-#[derive(PartialEq, Clone, Copy)]
-enum Cell {
-	Floor,
-	Empty,
-	Occupied
-}
+#[derive(PartialEq, Clone, Copy, Debug)]
+enum Cell { Inactive, Active }
 
 impl grid::FromChar for Cell {
 	fn from_c(c: char) -> Cell {
 		match c {
-			'.' => Cell::Floor,
-			'L' => Cell::Empty,
-			'#' => Cell::Occupied,
+			'.' => Cell::Inactive,
+			'#' => Cell::Active,
 			_ => panic!("Input char '{}' is not a valid Cell", c)
 		}
 	}
@@ -28,147 +28,94 @@ impl grid::FromChar for Cell {
 impl grid::ToStr for Cell {
 	fn to_s(&self) -> &'static str {
 		match self {
-			Cell::Floor => ".",
-			Cell::Empty => "L",
-			Cell::Occupied => "#",
+			Cell::Inactive => ".",
+			Cell::Active => "#",
 		}
 	}
 }
 
-fn solve(grid: Vec<Vec<Cell>>) -> usize {
-	advance_until_settled(grid)
-		.iter()
-		.flat_map(|row| row.iter())
-		.filter(|c| **c == Cell::Occupied)
+fn solve(mut cube: Cube<Cell>) -> usize {
+	for _ in 0..6 {
+		cube = tick(cube);
+	}
+
+	cube.cells.values()
+		.filter(|c| **c == Cell::Active)
 		.count()
+}
+
+fn tick(cube: Cube<Cell>) -> Cube<Cell> {
+	let xrange = cube.xrange();
+	let yrange = cube.yrange();
+	let zrange = cube.zrange();
+	let mut result = Cube::new(Cell::Inactive);
+
+	for x in (xrange.start() - 1)..=(xrange.end() + 1) {
+		for y in (yrange.start() - 1)..=(yrange.end() + 1) {
+			for z in (zrange.start() - 1)..=(zrange.end() + 1) {
+				let c = Coord {x, y, z};
+				let a = adj_active(&cube, c);
+
+				if cube[c] == Cell::Active && (a < 2 || a > 3) {
+					result[c] = Cell::Inactive;
+				} else if cube[c] == Cell::Inactive && a == 3 {
+					result[c] = Cell::Active;
+				} else {
+					result[c] = cube[c];
+				}
+			}
+		}
+	}
+
+	result
+}
+
+fn adj_active(cube: &Cube<Cell>, coord: Coord) -> usize {
+	lazy_static! {
+		static ref DELTAS: Vec<Coord> = {
+			let mut deltas = Vec::new();
+
+			for x in -1..=1 {
+				for y in -1..=1 {
+					for z in -1..=1 {
+						if x != 0 || y != 0 || z != 0 {
+							deltas.push(Coord {x, y, z});
+						}
+					}
+				}
+			}
+
+			deltas
+		};
+	}
+
+	DELTAS.iter().filter(|d| cube[coord + **d] == Cell::Active).count()
 }
 
 #[test]
 fn test_solve() {
-	let initial = grid::parse("
-L.LL.LL.LL
-LLLLLLL.LL
-L.L.L..L..
-LLLL.LL.LL
-L.LL.LL.LL
-L.LLLLL.LL
-..L.L.....
-LLLLLLLLLL
-L.LLLLLL.L
-L.LLLLL.LL
-");
-	assert_eq!(solve(initial), 37);
-}
-
-fn advance_until_settled(mut grid: Vec<Vec<Cell>>) -> Vec<Vec<Cell>> {
-	loop {
-		let (next_grid, any_changed) = tick(grid);
-
-		if any_changed {
-			grid = next_grid;
-		} else {
-			return next_grid;
-		}
-	}
-}
-
-
-fn tick(grid: Vec<Vec<Cell>>) -> (Vec<Vec<Cell>>, bool) {
-	let mut any_changed = false;
-	let result_grid = (0..grid.len())
-		.map(|i| {
-			(0..grid[i].len())
-				.map(|j| {
-					let c = grid[i][j];
-					if c == Cell::Empty && adj_occupied(&grid, i, j) == 0 {
-						any_changed = true;
-						Cell::Occupied
-					} else if c == Cell::Occupied && adj_occupied(&grid, i, j) >= 4 {
-						any_changed = true;
-						Cell::Empty
-					} else {
-						c
-					}
-				})
-				.collect()
-		})
-		.collect();
-	(result_grid, any_changed)
-}
-
-fn adj_occupied(grid: &Vec<Vec<Cell>>, i: usize, j: usize) -> usize {
-	static DELTAS: &[(i32, i32); 8] = &[
-		(-1, -1), (0, -1), (1, -1),
-		(-1, 0), (1, 0),
-		(-1, 1), (0, 1), (1, 1)
-	];
-
-	DELTAS.iter()
-		.filter(|d| {
-			let ni = i as i32 + d.0;
-			let nj = j as i32 + d.1;
-
-			if ni < 0 || nj < 0 || ni >= grid.len() as i32 || nj >= grid[i].len() as i32 {
-				false // Out of bounds
-			} else {
-				grid[ni as usize][nj as usize] == Cell::Occupied
-			}
-		})
-		.count()
+	let initial = Cube::parse_plane("
+.#.
+..#
+###
+", Cell::Inactive);
+	assert_eq!(solve(initial), 112);
 }
 
 #[test]
-fn test_tick() {
-	let initial = grid::parse("
-L.LL.LL.LL
-LLLLLLL.LL
-L.L.L..L..
-LLLL.LL.LL
-L.LL.LL.LL
-L.LLLLL.LL
-..L.L.....
-LLLLLLLLLL
-L.LLLLLL.L
-L.LLLLL.LL
-");
-	let tick1 = tick(initial).0;
-	assert_eq_grids!(tick1, grid::parse("
-#.##.##.##
-#######.##
-#.#.#..#..
-####.##.##
-#.##.##.##
-#.#####.##
-..#.#.....
-##########
-#.######.#
-#.#####.##
-"));
-	let tick2 = tick(tick1).0;
-	assert_eq_grids!(tick2, grid::parse("
-#.LL.L#.##
-#LLLLLL.L#
-L.L.L..L..
-#LLL.LL.L#
-#.LL.LL.LL
-#.LLLL#.##
-..L.L.....
-#LLLLLLLL#
-#.LLLLLL.L
-#.#LLLL.##
-"));	
-}
-
-
-#[test]
-fn test_parse_grid() {
-	let input = "
-#.
-L#
-";
-	let expected = vec![
-		vec![Cell::Occupied, Cell::Floor],
-		vec![Cell::Empty, Cell::Occupied],
-	];
-	assert_eq_grids!(grid::parse(input), expected);
+fn test_adj_active_single_plane() {
+	let cube = Cube::parse_plane("
+.#.
+..#
+###
+", Cell::Inactive);
+	assert_eq!(adj_active(&cube, Coord{x: 0, y: 0, z: 0}), 1);
+	assert_eq!(adj_active(&cube, Coord{x: 1, y: 0, z: 0}), 1);
+	assert_eq!(adj_active(&cube, Coord{x: 2, y: 0, z: 0}), 2);
+	assert_eq!(adj_active(&cube, Coord{x: 0, y: 1, z: 0}), 3);
+	assert_eq!(adj_active(&cube, Coord{x: 1, y: 1, z: 0}), 5);
+	assert_eq!(adj_active(&cube, Coord{x: 2, y: 1, z: 0}), 3);
+	assert_eq!(adj_active(&cube, Coord{x: 0, y: 2, z: 0}), 1);
+	assert_eq!(adj_active(&cube, Coord{x: 1, y: 2, z: 0}), 3);
+	assert_eq!(adj_active(&cube, Coord{x: 2, y: 2, z: 0}), 2);
 }
