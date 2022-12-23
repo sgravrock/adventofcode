4441e87d5a96a88a1a07e7f152cae4ccae1ed645
Steve Gravrock <sdg@panix.com>
2022-12-23 08:53:55 -0800

    Day 14 part 2 (Javascript): from part 1

---
+++ 2022/js/day14p2/boot.mjs
@@ -0,0 +1,25 @@
+import {parseInput, showCave, showChanged, run} from './lib.mjs';
+
+document.querySelector('button').addEventListener('click', async function(e) {
+	e.preventDefault();
+	const cave = parseInput(document.querySelector('textarea').value);
+	const table = document.querySelector('table');
+	const showAnimations = document.querySelector('input[type=checkbox]').checked;
+	showCave(cave, table);
+	const startTime = new Date().getTime();
+	let lastAnimationTime = startTime;
+
+	const n = await run(cave, async (changed) => {
+		if (showAnimations) {
+			showChanged(cave, table, changed);
+			// 870s RAF
+			await new Promise(res => setTimeout(res));
+		}
+	});
+
+	if (!showAnimations) {
+		showCave(cave, table);
+	}
+
+	alert('Answer: ' + n + '. Time: ' + ((new Date().getTime() - startTime) / 1000) + 's');
+});
