7e17c9344e3921524277eca5525c394295853e73
Steve Gravrock <sdg@panix.com>
2022-12-23 12:26:58 -0800

    Day 14 part 2 (Javascript): multiple animation rates

--- 2022/js/day14p2/boot.mjs
+++ 2022/js/day14p2/boot.mjs
@@ -4,21 +4,38 @@ document.querySelector('button').addEventListener('click', async function(e) {
 	e.preventDefault();
 	const cave = parseInput(document.querySelector('textarea').value);
 	const table = document.querySelector('table');
-	const showAnimations = document.querySelector('input[type=checkbox]').checked;
+	const animations = document.querySelector('form').animations.value;
 	showCave(cave, table);
 	const startTime = new Date().getTime();
-	let lastAnimationTime = startTime;
+	let frameCount = 0;
+	let changedSinceLastFrame = [];
 
 	const n = await run(cave, async (changed) => {
-		if (showAnimations) {
-			showChanged(cave, table, changed);
-			await new Promise(res => setTimeout(res));
+		if (animations === 'none') {
+			return;
 		}
+
+		changedSinceLastFrame = [...changedSinceLastFrame, ...changed];
+		let animationRate;
+
+		if (animations === 'all') {
+			animationRate = 1;
+		} else {
+			animationRate = parseInt(animations, 10);
+		}
+
+		if (frameCount % animationRate === 0) {
+			showChanged(cave, table, changedSinceLastFrame);
+			changedSinceLastFrame = [];
+			await new Promise(res => requestAnimationFrame(res));
+		} else if (animationRate > 200 && frameCount % 200 === 0) {
+			// Tick anyway so the browser doesn't kill us
+			await new Promise(res => requestAnimationFrame(res));
+		}
+
+		frameCount++;
 	});
 
-	if (!showAnimations) {
-		showCave(cave, table);
-	}
-
+	showCave(cave, table);
 	alert('Answer: ' + n + '. Time: ' + ((new Date().getTime() - startTime) / 1000) + 's');
 });
