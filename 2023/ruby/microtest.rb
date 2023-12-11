8dea2ab23b1c7881a54ee8e563c33afd35a9b64b
Steve Gravrock <sdg@panix.com>
2023-12-10 19:03:48 -0800

    Day 10 part 2 (Ruby)

--- 2023/ruby/microtest.rb
+++ 2023/ruby/microtest.rb
@@ -2,24 +2,35 @@ module Microtest
 	def Microtest.run(tests)
 		n_tests = 0
 		n_failed = 0
-		tests.methods
-			.select {|symbol| symbol.to_s.start_with? 'test_' }
-			.each do |symbol|
-				tests.state = SingleTestState.new symbol.to_s
-				tests.run_one_test symbol
-				n_tests += 1
+		focused = true
+		test_methods = tests.methods
+			.select {|symbol| symbol.to_s.start_with? 'ftest_' }
 
-				unless tests.state.ok
-					n_failed += 1
-				end
+		if test_methods.empty?
+			focused = false
+			test_methods = tests.methods
+				.select {|symbol| symbol.to_s.start_with? 'test_' }
+		end
+
+		test_methods.each do |symbol|
+			tests.state = SingleTestState.new symbol.to_s
+			tests.run_one_test symbol
+			n_tests += 1
+
+			unless tests.state.ok
+				n_failed += 1
 			end
+		end
 
 		puts
-		if n_failed == 0
-			puts "All #{n_tests} tests passed."
-		else
+		if focused then
+			puts "#{n_failed} of #{n_tests} focused tests failed."
+			exit 2
+		elsif n_failed > 0
 			puts "#{n_failed} of #{n_tests} tests failed."
 			exit 1
+		else
+			puts "All #{n_tests} tests passed."
 		end
 	end
 
@@ -37,6 +48,16 @@ module Microtest
 			end
 		end
 
+		def fail(msg)
+			if state.ok
+				state.ok = false
+				puts "#{state.name} failed:"
+			end
+
+			puts "   #{msg}"
+		end
+
+		# TODO refactor, use fail()
 		def assert_equal(expected, actual)
 			if expected != actual
 				if state.ok
