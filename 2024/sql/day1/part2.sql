7a67cb8ef200a7ffc301faf996eb601a5b7e143b
Steve Gravrock <sdg@panix.com>
2024-12-01 12:17:37 -0800

    2024 day 1 part 2 (SQL): use CTE

--- 2024/sql/day1/part2.sql
+++ 2024/sql/day1/part2.sql
@@ -1,9 +1,7 @@
-create temporary table similarity (a integer primary key, score integer not null);
-
-insert into similarity (a, score)
-select l.a, count(*) * l.a 
-from input l
-inner join input r on r.b = l.a
-group by l.a;
-
+with similarity as (
+	select count(*) * l.a as score
+	from input l
+	inner join input r on r.b = l.a
+	group by l.a
+)
 select sum(score) from similarity;
